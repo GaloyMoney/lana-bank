81b3b23c85e284c8714a1b2f265491e78b84e100
vindard <17693119+vindard@users.noreply.github.com>
2024-06-04 23:32:20 -0400

    chore: add apollo studio cors headers

--- core/src/server/admin/mod.rs
+++ core/src/server/admin/mod.rs
@@ -4,7 +4,7 @@ mod config;
 
 use async_graphql::*;
 use async_graphql_axum::{GraphQLRequest, GraphQLResponse};
-use axum::{routing::get, Extension, Router};
+use axum::{http::HeaderValue, response::IntoResponse, routing::get, Extension, Router};
 use axum_extra::headers::HeaderMap;
 
 use crate::app::LavaApp;
@@ -17,7 +17,9 @@ pub async fn run(config: AdminServerConfig, app: LavaApp) -> anyhow::Result<()> 
     let app = Router::new()
         .route(
             "/graphql",
-            get(playground).post(axum::routing::post(graphql_handler)),
+            get(playground)
+                .post(axum::routing::post(graphql_handler))
+                .options(cors_preflight),
         )
         .layer(Extension(schema));
 
@@ -33,14 +35,43 @@ pub async fn graphql_handler(
     headers: HeaderMap,
     schema: Extension<Schema<graphql::Query, graphql::Mutation, EmptySubscription>>,
     req: GraphQLRequest,
-) -> GraphQLResponse {
+) -> impl IntoResponse {
     lava_tracing::http::extract_tracing(&headers);
     let req = req.into_inner();
-    schema.execute(req).await.into()
+    let response = schema.execute(req).await;
+    let mut res = GraphQLResponse::from(response).into_response();
+
+    res.headers_mut().insert(
+        "access-control-allow-origin",
+        HeaderValue::from_static("https://studio.apollographql.com"),
+    );
+    res.headers_mut().insert(
+        "access-control-allow-credentials",
+        HeaderValue::from_static("true"),
+    );
+
+    res
 }
 
 async fn playground() -> impl axum::response::IntoResponse {
     axum::response::Html(async_graphql::http::playground_source(
         async_graphql::http::GraphQLPlaygroundConfig::new("/graphql"),
     ))
+}
+
+async fn cors_preflight() -> impl IntoResponse {
+    axum::response::Response::builder()
+        .header(
+            "access-control-allow-origin",
+            "https://studio.apollographql.com",
+        )
+        .header("access-control-allow-methods", "POST, OPTIONS")
+        .header("access-control-allow-credentials", "true")
+        .header(
+            "access-control-allow-headers",
+            "Content-Type, Authorization",
+        )
+        .status(204)
+        .body(axum::body::Body::empty())
+        .unwrap()
 }
