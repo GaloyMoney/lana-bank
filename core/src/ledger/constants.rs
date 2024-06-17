e91f1a3aea9bf1b6ef9ad958b4af3eaf8997ee7d
vindard <17693119+vindard@users.noreply.github.com>
2024-06-17 15:00:25 -0400

    chore: remove unused usdt-cash asset account

--- core/src/ledger/constants.rs
+++ core/src/ledger/constants.rs
@@ -12,9 +12,6 @@ pub(super) const BITFINEX_USDT_CASH_INTEGRATION_ID: Uuid =
     uuid!("00000000-0000-0000-0000-200000000002");
 pub(super) const BITFINEX_USDT_CASH_INTEGRATION_NAME: &str = "Usdt Cash Bitfinex Integration";
 
-// Accounts
-pub(super) const _BANK_USDT_CASH_ID: Uuid = uuid!("00000000-0000-0000-0000-000000000004");
-
 // Templates
 pub(super) const APPROVE_LOAN_CODE: &str = "APPROVE_LOAN";
 pub(super) const INCUR_INTEREST_CODE: &str = "INCUR_INTEREST";
