146fddb3065bfae29246e675387031dc5424f5d3
sandipndev <hey@sandipan.dev>
2024-06-27 03:38:43 +0530

    chore: customer portal reads jwt

--- apps/customer-portal/env.ts
+++ apps/customer-portal/env.ts
@@ -5,9 +5,11 @@ export const env = createEnv({
   shared: {
     NEXT_PUBLIC_CORE_URL: z.string().url().default("http://localhost:5252"),
     NEXT_PUBLIC_KRATOS_PUBLIC_API: z.string().url().default("http://localhost:4433"),
+    JWKS_URL: z.string().url().default("http://localhost:4456/.well-known/jwks.json"),
   },
   runtimeEnv: {
     NEXT_PUBLIC_CORE_URL: process.env.NEXT_PUBLIC_CORE_URL,
     NEXT_PUBLIC_KRATOS_PUBLIC_API: process.env.NEXT_PUBLIC_KRATOS_PUBLIC_API,
+    JWKS_URL: process.env.JWKS_URL,
   },
 })
--- apps/customer-portal/middleware.ts
+++ apps/customer-portal/middleware.ts
@@ -1,43 +1,17 @@
 import { NextRequest, NextResponse } from "next/server"
 
-import { authService } from "./lib/auth"
-
-const privateRoutes = ["/"]
+import { verifyToken } from "./lib/auth/jwks"
 
 export async function middleware(request: NextRequest): Promise<NextResponse | void> {
-  const isPrivateRoute = privateRoutes.some((route) => request.nextUrl.pathname === route)
-  const isAuthRoute = request.nextUrl.pathname.split("/")[1] === "auth"
+  const token = request.headers.get("authorization")
 
-  if (isAuthRoute) {
-    const cookieParam = request.cookies
-      .getAll()
-      .reduce((acc, cookie) => `${acc}${cookie.name}=${cookie.value}; `, "")
-    const response = await authService().getSession({
-      cookie: cookieParam,
-    })
-    if (response instanceof Error) {
-      return NextResponse.next()
-    }
-    if (response.data) {
-      return NextResponse.redirect(new URL("/", request.url))
-    }
-    return NextResponse.next()
-  }
+  /* Shouldn't happen unless requests are not coming through oathkeeper */
+  if (!token) throw new Error("Authorization header not found")
 
-  if (isPrivateRoute) {
-    const cookieParam = request.cookies
-      .getAll()
-      .reduce((acc, cookie) => `${acc}${cookie.name}=${cookie.value}; `, "")
-    const response = await authService().getSession({
-      cookie: cookieParam,
-    })
-    if (response instanceof Error) {
-      return NextResponse.redirect(new URL("/auth", request.url))
-    }
-    if (!response.data) {
-      return NextResponse.redirect(new URL("/auth", request.url))
-    }
-    return NextResponse.next()
+  const decodedToken = await verifyToken(token.split(" ")[1])
+
+  if (decodedToken.sub === "anonymous") {
+    return NextResponse.redirect(new URL("/auth", request.url))
   }
 
   return NextResponse.next()
