a6379e0b4e9cf26203483ffa1cea39231576b17b
sandipndev <hey@sandipan.dev>
2024-06-28 03:53:28 +0530

    chore: use 127.0.0.1 only

--- apps/customer-portal/env.ts
+++ apps/customer-portal/env.ts
@@ -3,8 +3,8 @@ import { z } from "zod"
 
 export const env = createEnv({
   shared: {
-    NEXT_PUBLIC_CORE_URL: z.string().url().default("http://localhost:5252"),
-    NEXT_PUBLIC_KRATOS_PUBLIC_API: z.string().url().default("http://localhost:4455"),
+    NEXT_PUBLIC_CORE_URL: z.string().url().default("http://127.0.0.1:5252"),
+    NEXT_PUBLIC_KRATOS_PUBLIC_API: z.string().url().default("http://127.0.0.1:4455"),
     JWKS_URL: z.string().url().default("http://localhost:4456/.well-known/jwks.json"),
   },
   runtimeEnv: {
