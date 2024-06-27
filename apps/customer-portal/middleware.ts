dad80521f477582ff2a8e0f03036f769f49313e7
sandipndev <hey@sandipan.dev>
2024-06-27 18:13:03 +0530

    chore: customer portal private routes

--- apps/customer-portal/middleware.ts
+++ apps/customer-portal/middleware.ts
@@ -2,15 +2,18 @@ import { NextRequest, NextResponse } from "next/server"
 
 import { verifyToken } from "./lib/auth/jwks"
 
+const privateRoutes = ["/"]
+
 export async function middleware(request: NextRequest): Promise<NextResponse | void> {
   const token = request.headers.get("authorization")
 
-  /* Shouldn't happen unless requests are not coming through oathkeeper */
+  /* Next two lines shouldn't throw errors unless requests are not coming through oathkeeper or JWKS_URL is invalid */
   if (!token) throw new Error("Authorization header not found")
-
   const decodedToken = await verifyToken(token.split(" ")[1])
 
-  if (decodedToken.sub === "anonymous") {
+  const isPrivateRoute = privateRoutes.some((route) => request.nextUrl.pathname === route)
+
+  if (isPrivateRoute && decodedToken.sub === "anonymous") {
     return NextResponse.redirect(new URL("/auth", request.url))
   }
 
