20f22cad506253447bd05443fee36772c1d653fc
sandipndev <hey@sandipan.dev>
2024-07-08 23:45:23 +0530

    feat: chart of accounts page

---
+++ apps/admin-panel/app/chart-of-accounts/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+import React from "react"
+
+import { PageHeading } from "@/components/page-heading"
+
+function ChartOfAccountsPage() {
+  return (
+    <main>
+      <PageHeading>Chart of Accounts</PageHeading>
+    </main>
+  )
+}
+
+export default ChartOfAccountsPage
