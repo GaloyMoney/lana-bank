f1f6a4ab039792ee0421cbe0e1bb9c5056fca90a
vindard <17693119+vindard@users.noreply.github.com>
2024-07-12 15:16:18 -0400

    chore: remove redundant loan-related account sets

--- tf/balance_sheet.tf
+++ tf/balance_sheet.tf
@@ -36,27 +36,14 @@ resource "cala_account_set_member_account" "bfx_deposits" {
   member_account_id = cala_bitfinex_integration.bank_deposit.omnibus_account_id
 }
 
-resource "cala_account_set" "loans" {
-  id                  = "00000000-0000-0000-0000-900000000001"
-  journal_id          = cala_journal.journal.id
-  name                = "Loans"
-  normal_balance_type = "DEBIT"
-}
-
 resource "cala_account_set_member_account_set" "loans" {
   account_set_id        = cala_balance_sheet.lava.schedule9_account_set_id
-  member_account_set_id = cala_account_set.loans.id
-}
-
-resource "cala_account_set" "interest_revenue" {
-  id         = "00000000-0000-0000-0000-500000000001"
-  journal_id = cala_journal.journal.id
-  name       = "Interest Revenue"
+  member_account_set_id = cala_account_set.loans_receivable_control.id
 }
 
 resource "cala_account_set_member_account_set" "interest_revenue" {
   account_set_id        = cala_balance_sheet.lava.schedule5_account_set_id
-  member_account_set_id = cala_account_set.interest_revenue.id
+  member_account_set_id = cala_account_set.interest_revenue_control.id
 }
 
 resource "cala_account_set" "reserves" {
