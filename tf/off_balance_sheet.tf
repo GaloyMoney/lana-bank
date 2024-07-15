4cff843e50e9288000416a12daa439acfe847b97
vindard <17693119+vindard@users.noreply.github.com>
2024-07-15 16:06:22 -0400

    chore: add separate off-balance-sheet account sets

--- tf/off_balance_sheet.tf
+++ tf/off_balance_sheet.tf
@@ -1,3 +1,18 @@
+# Off-Balance-Sheet "Chart of Accounts" Account Set
+resource "cala_account_set" "obs_chart_of_accounts" {
+  id         = "10000000-0000-0000-0000-100000000001"
+  journal_id = cala_journal.journal.id
+  name       = "Off-Balance-Sheet Chart of Accounts"
+}
+
+resource "cala_account_set" "obs_trial_balance" {
+  id                  = "10000000-0000-0000-0000-100000000002"
+  journal_id          = cala_journal.journal.id
+  name                = "Off-Balance-Sheet Trial Balance"
+  normal_balance_type = "DEBIT"
+}
+
+
 resource "random_uuid" "collateral_deposits_control" {}
 resource "cala_account_set" "collateral_deposits_control" {
   id                  = random_uuid.collateral_deposits_control.result
@@ -5,8 +20,12 @@ resource "cala_account_set" "collateral_deposits_control" {
   name                = "Off-Balance-Sheet Deposits For Collateral From Users Control Account"
   normal_balance_type = "DEBIT"
 }
-resource "cala_account_set_member_account_set" "collateral_deposits_control_in_trial_balance" {
-  account_set_id        = cala_account_set.trial_balance.id
+resource "cala_account_set_member_account_set" "collateral_deposits_control_in_obs_chart_of_accounts" {
+  account_set_id        = cala_account_set.obs_chart_of_accounts.id
+  member_account_set_id = cala_account_set.collateral_deposits_control.id
+}
+resource "cala_account_set_member_account_set" "collateral_deposits_control_in_obs_trial_balance" {
+  account_set_id        = cala_account_set.obs_trial_balance.id
   member_account_set_id = cala_account_set.collateral_deposits_control.id
 }
 
@@ -16,8 +35,12 @@ resource "cala_account_set" "user_collateral_control" {
   name                = "User Collateral Control Account"
   normal_balance_type = "CREDIT"
 }
-resource "cala_account_set_member_account_set" "user_collateral_control_in_trial_balance" {
-  account_set_id        = cala_account_set.trial_balance.id
+resource "cala_account_set_member_account_set" "user_collateral_control_in_obs_chart_of_accounts" {
+  account_set_id        = cala_account_set.obs_chart_of_accounts.id
+  member_account_set_id = cala_account_set.user_collateral_control.id
+}
+resource "cala_account_set_member_account_set" "user_collateral_control_in_obs_trial_balance" {
+  account_set_id        = cala_account_set.obs_trial_balance.id
   member_account_set_id = cala_account_set.user_collateral_control.id
 }
 
@@ -27,8 +50,11 @@ resource "cala_account_set" "loans_collateral_control" {
   name                = "Loans Collateral Control Account"
   normal_balance_type = "CREDIT"
 }
-resource "cala_account_set_member_account_set" "loans_collateral_control_in_trial_balance" {
-  account_set_id        = cala_account_set.trial_balance.id
+resource "cala_account_set_member_account_set" "loans_collateral_control_in_obs_chart_of_accounts" {
+  account_set_id        = cala_account_set.obs_chart_of_accounts.id
   member_account_set_id = cala_account_set.loans_collateral_control.id
 }
-
+resource "cala_account_set_member_account_set" "loans_collateral_control_in_obs_trial_balance" {
+  account_set_id        = cala_account_set.obs_trial_balance.id
+  member_account_set_id = cala_account_set.loans_collateral_control.id
+}
