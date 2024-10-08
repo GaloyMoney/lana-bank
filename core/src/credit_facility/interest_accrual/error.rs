db215b9bc7a941d1aaaae1a9898941348df8ec3b
vindard <17693119+vindard@users.noreply.github.com>
2024-10-08 15:37:36 -0400

    feat: implement initiate_incurrence function in InterestAccrual

--- core/src/credit_facility/interest_accrual/error.rs
+++ core/src/credit_facility/interest_accrual/error.rs
@@ -8,4 +8,10 @@ pub enum InterestAccrualError {
     EntityError(#[from] crate::entity::EntityError),
     #[error("InterestAccrualError - JobError: {0}")]
     JobError(#[from] crate::job::error::JobError),
+    #[error("InterestAccrualError - AlreadyAccrued")]
+    AlreadyAccrued,
+    #[error("InterestAccrualError - InterestPeriodStartDatePastAccrualDate")]
+    InterestPeriodStartDatePastAccrualDate,
+    #[error("InterestAccrualError - NonCurrentIncurrencePeriod")]
+    NonCurrentIncurrencePeriod,
 }
