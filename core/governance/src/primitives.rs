7db5efccb792e235d539f79c7e39d361d63d3499
bodymindarts <justin@galoy.io>
2024-10-27 09:00:22 +0100

    chore: put object + action into rbac

--- core/governance/src/primitives.rs
+++ core/governance/src/primitives.rs
@@ -1,6 +1,6 @@
 use std::{fmt::Display, str::FromStr};
 
-pub use shared_primitives::{CommitteeId, UserId};
+pub use shared_primitives::{AllOrOne, CommitteeId, UserId};
 
 #[derive(Clone, Copy, Debug, PartialEq, strum::EnumDiscriminants)]
 #[strum_discriminants(derive(strum::Display, strum::EnumString))]
@@ -38,10 +38,39 @@ pub enum CommitteeAction {
     Create,
 }
 
-#[derive(PartialEq, Clone, Copy, Debug, strum::Display, strum::EnumString)]
-#[strum(serialize_all = "kebab-case")]
+pub type CommitteeAllOrOne = AllOrOne<CommitteeId>;
+
+#[derive(Clone, Copy, Debug, PartialEq, strum::EnumDiscriminants)]
+#[strum_discriminants(derive(strum::Display, strum::EnumString))]
+#[strum_discriminants(strum(serialize_all = "kebab-case"))]
 pub enum GovernanceObject {
-    Committee,
+    Committee(CommitteeAllOrOne),
+}
+
+impl Display for GovernanceObject {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let discriminant = GovernanceObjectDiscriminants::from(self);
+        use GovernanceObject::*;
+        match self {
+            Committee(obj_ref) => write!(f, "{}/{}", discriminant, obj_ref),
+        }
+    }
+}
+
+impl FromStr for GovernanceObject {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (entity, id) = s.split_once('/').expect("missing slash");
+        use GovernanceObjectDiscriminants::*;
+        let res = match entity.parse().expect("invalid entity") {
+            Committee => {
+                let obj_ref = id.parse().map_err(|_| "could not parse GovernanceObject")?;
+                GovernanceObject::Committee(obj_ref)
+            }
+        };
+        Ok(res)
+    }
 }
 
 impl From<CommitteeAction> for GovernanceAction {
--- core/rbac/src/lib.rs
+++ core/rbac/src/lib.rs
@@ -1,5 +1,8 @@
 #![cfg_attr(feature = "fail-on-warnings", deny(warnings))]
 #![cfg_attr(feature = "fail-on-warnings", deny(clippy::all))]
+
 mod action;
+mod object;
 
 pub use action::*;
+pub use object::*;
