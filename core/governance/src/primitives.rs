d0fd6a4688989a59f4d310b31cdd7875bf928a54
bodymindarts <justin@galoy.io>
2024-10-27 08:18:34 +0100

    chore: initial core/rbac

--- core/governance/src/primitives.rs
+++ core/governance/src/primitives.rs
@@ -1,3 +1,5 @@
+use std::{fmt::Display, str::FromStr};
+
 pub use shared_primitives::{CommitteeId, UserId};
 
 #[derive(Clone, Copy, Debug, PartialEq, strum::EnumDiscriminants)]
@@ -5,6 +7,29 @@ pub use shared_primitives::{CommitteeId, UserId};
 #[strum_discriminants(strum(serialize_all = "kebab-case"))]
 pub enum GovernanceAction {
     Committee(CommitteeAction),
+}
+
+impl Display for GovernanceAction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}:", GovernanceActionDiscriminants::from(self))?;
+        use GovernanceAction::*;
+        match self {
+            Committee(action) => action.fmt(f),
+        }
+    }
+}
+
+impl FromStr for GovernanceAction {
+    type Err = strum::ParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (entity, action) = s.split_once(':').expect("missing colon");
+        use GovernanceActionDiscriminants::*;
+        let res = match entity.parse()? {
+            Committee => GovernanceAction::from(action.parse::<CommitteeAction>()?),
+        };
+        Ok(res)
+    }
 }
 
 #[derive(PartialEq, Clone, Copy, Debug, strum::Display, strum::EnumString)]
