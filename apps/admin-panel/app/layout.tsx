aec43d9940e69945b28f1bfd7c3e4bbb1783ef7a
siddharth <siddharthtiwarikreplind@gmail.com>
2024-11-05 11:14:32 +0530

    chore: adding next-auth

--- apps/admin-panel/app/layout.tsx
+++ apps/admin-panel/app/layout.tsx
@@ -3,20 +3,12 @@ import { Toast } from "@/components/toast"
 import { HelveticaNeueFont, RobotoMono } from "@/lib/ui/fonts"
 import "@/lib/ui/globals.css"
 
-const RootLayout: React.FC<React.PropsWithChildren> = ({ children }) => (
-  <html lang="en">
-    <body
-      className={`${HelveticaNeueFont.variable} ${RobotoMono.variable} antialiased w-screen h-screen select-none`}
-    >
-      <Toast />
-      {children}
-    </body>
-  </html>
-)
+import { AuthSessionProvider } from "./session-provider"
+import { getServerSession } from "next-auth"
 
-export default RootLayout
-
-import type { Metadata } from "next"
+import { Metadata } from "next/types"
+import { redirect } from "next/navigation"
+import { authOptions } from "./api/auth/[...nextauth]/options"
 
 export const metadata: Metadata = {
   title: "Lana Bank | Admin Panel",
@@ -27,3 +19,29 @@ export const metadata: Metadata = {
     },
   ],
 }
+
+const RootLayout = async ({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) => {
+  const session = await getServerSession(authOptions)
+  if (!session) {
+    redirect("/api/auth/signin")
+  }
+
+  return (
+    <html lang="en">
+      <AuthSessionProvider session={session}>
+        <body
+          className={`${HelveticaNeueFont.variable} ${RobotoMono.variable} antialiased w-screen h-screen select-none`}
+        >
+          <Toast />
+          {children}
+        </body>
+      </AuthSessionProvider>
+    </html>
+  )
+}
+
+export default RootLayout
