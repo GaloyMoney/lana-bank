aa5dc4664352b1924b3f34b3d95e425239a7fae7
vindard <17693119+vindard@users.noreply.github.com>
2024-11-21 11:48:52 -0400

    fix: temporarily remove problematic Debug trait

--- lava/app/src/credit_facility/mod.rs
+++ lava/app/src/credit_facility/mod.rs
@@ -487,13 +487,13 @@ impl CreditFacilities {
         Ok(credit_facility)
     }
 
-    #[instrument(name = "credit_facility.list", skip(self), err)]
+    // #[instrument(name = "credit_facility.list", skip(self), err)]
     pub async fn list(
         &self,
         sub: &Subject,
         query: es_entity::PaginatedQueryArgs<CreditFacilitiesCursor>,
         filter: FindManyCreditFacilities,
-        sort: impl Into<Sort<CreditFacilitiesSortBy>> + std::fmt::Debug,
+        sort: impl Into<Sort<CreditFacilitiesSortBy>>,
     ) -> Result<
         es_entity::PaginatedQueryRet<CreditFacility, CreditFacilitiesCursor>,
         CreditFacilityError,
