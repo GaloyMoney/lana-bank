97443cedfa5647ae738445652cb22fc0648fb17c
vindard <17693119+vindard@users.noreply.github.com>
2024-12-12 16:05:16 -0400

    feat: add chart of accounts repo

--- core/chart_of_accounts/src/chart_of_accounts/error.rs
+++ core/chart_of_accounts/src/chart_of_accounts/error.rs
@@ -2,6 +2,14 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum ChartOfAccountError {
+    #[error("ChartOfAccountError - Sqlx: {0}")]
+    Sqlx(#[from] sqlx::Error),
+    #[error("ChartOfAccountError - EsEntityError: {0}")]
+    EsEntityError(es_entity::EsEntityError),
+    #[error("ChartOfAccountError - CursorDestructureError: {0}")]
+    CursorDestructureError(#[from] es_entity::CursorDestructureError),
     #[error("ChartOfAccountError - InvalidChartOfAccountCodeStr")]
     InvalidChartOfAccountCodeStr,
 }
+
+es_entity::from_es_entity_error!(ChartOfAccountError);
--- core/chart_of_accounts/src/chart_of_accounts/mod.rs
+++ core/chart_of_accounts/src/chart_of_accounts/mod.rs
@@ -1,3 +1,4 @@
 mod code;
 mod entity;
 mod error;
+mod repo;
