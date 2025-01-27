a04ae15be19c8be75d3eec833a607b6f21bdae20
Sandipan Dey <hey@sandipan.dev>
2025-01-28 02:21:21 +0530

    test: 100 years

--- apps/admin-panel/cypress/e2e/credit-facilities.cy.ts
+++ apps/admin-panel/cypress/e2e/credit-facilities.cy.ts
@@ -15,7 +15,7 @@ describe("credit facility", () => {
       marginCallCvl: "120",
       initialCvl: "140",
       duration: {
-        units: 12 * 10,
+        units: 12 * 100,
         period: Period.Months,
       },
     }).then((id) => {
