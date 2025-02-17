c9f082a9e2e1040969f708642a0e5fe2598f6c50
Jose Ignacio Rojas <jir@galoy.io>
2025-02-17 16:57:47 -0700

    chore: fmt

--- tf/bq-setup/main.tf
+++ tf/bq-setup/main.tf
@@ -12,19 +12,19 @@ variable "additional_owners" {
 }
 
 locals {
-  name_prefix            = var.name_prefix
-  holistics_sa_name      = "${var.name_prefix}-holistics"
-  gcp_project            = var.gcp_project
-  gcp_region             = var.gcp_region
+  name_prefix       = var.name_prefix
+  holistics_sa_name = "${var.name_prefix}-holistics"
+  gcp_project       = var.gcp_project
+  gcp_region        = var.gcp_region
 
   additional_owners = var.additional_owners
 
-  dataset_id             = "${replace(local.name_prefix, "-", "_")}_dataset"
-  sa_account_id          = replace("${var.name_prefix}-lana-bq-access", "_", "-")
-  git_token              = var.git_token
-  git_token_secret_id    = "${var.name_prefix}-git-token"
+  dataset_id          = "${replace(local.name_prefix, "-", "_")}_dataset"
+  sa_account_id       = replace("${var.name_prefix}-lana-bq-access", "_", "-")
+  git_token           = var.git_token
+  git_token_secret_id = "${var.name_prefix}-git-token"
 
-  dbt_dataset_name       = replace("dbt_${local.name_prefix}", "-", "_")
-  location               = "US"
-  docs_bucket_name       = "${var.name_prefix}-lana-documents"
+  dbt_dataset_name = replace("dbt_${local.name_prefix}", "-", "_")
+  location         = "US"
+  docs_bucket_name = "${var.name_prefix}-lana-documents"
 }
