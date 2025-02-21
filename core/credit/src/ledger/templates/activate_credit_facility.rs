fadd5364142e368778a33e22676ffb249bb6f9dd
vindard <17693119+vindard@users.noreply.github.com>
2025-02-21 10:43:03 -0400

    chore: mirror changes to core/credit

--- core/credit/src/ledger/templates/activate_credit_facility.rs
+++ core/credit/src/ledger/templates/activate_credit_facility.rs
@@ -6,7 +6,7 @@ use cala_ledger::{
     *,
 };
 
-use crate::credit_facility::ledger::error::*;
+use crate::ledger::error::*;
 
 pub const ACTIVATE_CREDIT_FACILITY_CODE: &str = "ACTIVATE_CREDIT_FACILITY";
 
