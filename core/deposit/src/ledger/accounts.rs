a6c703bebca2cee1d6dbbbbbd58f9b3dfb3cf46e
bodymindarts <justin@galoy.io>
2025-03-06 16:31:12 +0100

    chore: use LeafAccountFactory in deposits

--- core/deposit/src/ledger/accounts.rs
+++ core/deposit/src/ledger/accounts.rs
@@ -1,11 +1,4 @@
-use chart_of_accounts::TransactionAccountFactory;
-
 use crate::primitives::LedgerAccountId;
-
-#[derive(Clone)]
-pub struct DepositAccountFactories {
-    pub deposits: TransactionAccountFactory,
-}
 
 #[derive(Clone)]
 pub struct DepositOmnibusAccountIds {
