8ff42c69600a268b36322113b2b002ba795b8b5a
bodymindarts <justin@galoy.io>
2025-03-10 10:53:09 +0100

    test: remove chart of accounts from deposit / withdraw test

--- core/deposit/tests/deposit.rs
+++ core/deposit/tests/deposit.rs
@@ -4,14 +4,11 @@ use rust_decimal_macros::dec;
 
 use authz::dummy::DummySubject;
 use cala_ledger::{CalaLedger, CalaLedgerConfig};
-use chart_of_accounts::{ChartCategory, CoreChartOfAccounts};
 use deposit::*;
 use helpers::{action, event, object};
 
 #[tokio::test]
 async fn deposit() -> anyhow::Result<()> {
-    use rand::Rng;
-
     let pool = helpers::init_pool().await?;
 
     let outbox = outbox::Outbox::<event::DummyEvent>::init(&pool).await?;
@@ -28,76 +25,6 @@ async fn deposit() -> anyhow::Result<()> {
     let jobs = job::Jobs::new(&pool, job::JobExecutorConfig::default());
 
     let journal_id = helpers::init_journal(&cala).await?;
-
-    let chart_id = ChartId::new();
-    let chart_of_accounts = CoreChartOfAccounts::init(&pool, &authz, &cala, journal_id).await?;
-    chart_of_accounts
-        .create_chart(
-            chart_id,
-            "Test Chart".to_string(),
-            format!("{:02}", rand::thread_rng().gen_range(0..100)),
-        )
-        .await?;
-
-    let control_account = chart_of_accounts
-        .create_control_account(
-            LedgerAccountSetId::new(),
-            chart_id,
-            ChartCategory::Liabilities,
-            "Deposits".to_string(),
-            "deposits".to_string(),
-        )
-        .await?;
-    let deposits_name = "User Deposits";
-    let deposits_reference = format!(
-        "user-deposits:{:04}",
-        rand::thread_rng().gen_range(0..10000)
-    );
-    let _control_sub_account = chart_of_accounts
-        .create_control_sub_account(
-            chart_id,
-            control_account,
-            deposits_name.to_string(),
-            deposits_reference,
-        )
-        .await?;
-
-    let omnibus_control_account = chart_of_accounts
-        .create_control_account(
-            LedgerAccountSetId::new(),
-            chart_id,
-            ChartCategory::Assets,
-            "Deposits Omnibus".to_string(),
-            "deposits-omnibus".to_string(),
-        )
-        .await?;
-    let omnibus_name = "User Deposits Omnibus";
-    let omnibus_reference = &format!(
-        "user-deposits-omnibus:{:04}",
-        rand::thread_rng().gen_range(0..10000)
-    );
-    let omnibus_control_sub_account = chart_of_accounts
-        .create_control_sub_account(
-            chart_id,
-            omnibus_control_account,
-            omnibus_name.to_string(),
-            omnibus_reference.to_string(),
-        )
-        .await?;
-    let omnibus_factory =
-        chart_of_accounts.transaction_account_factory(omnibus_control_sub_account);
-    let omnibus_account_id = LedgerAccountId::new();
-    let mut op = cala.begin_operation().await?;
-    omnibus_factory
-        .create_transaction_account_in_op(
-            &mut op,
-            omnibus_account_id,
-            omnibus_reference,
-            omnibus_name,
-            omnibus_name,
-        )
-        .await?;
-    op.commit().await?;
 
     let deposit = CoreDeposit::init(
         &pool,
