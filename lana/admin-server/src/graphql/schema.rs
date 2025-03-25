82210c366b8c83297beb540cd9bded7c1256035d
Vaibhav <thevaibhavdixit@gmail.com>
2025-03-25 13:54:37 +0530

    feat: interest receivables account sets

--- lana/admin-server/src/graphql/schema.rs
+++ lana/admin-server/src/graphql/schema.rs
@@ -995,11 +995,6 @@ impl Mutation {
             .chart_of_account_collateral_parent_code(
                 input.chart_of_account_collateral_parent_code.parse()?,
             )
-            .chart_of_account_interest_receivable_parent_code(
-                input
-                    .chart_of_account_interest_receivable_parent_code
-                    .parse()?,
-            )
             .chart_of_account_interest_income_parent_code(
                 input.chart_of_account_interest_income_parent_code.parse()?,
             )
@@ -1020,6 +1015,22 @@ impl Mutation {
             .chart_of_account_long_term_financial_institution_disbursed_receivable_parent_code(input.chart_of_account_long_term_financial_institution_disbursed_receivable_parent_code.parse()?)
             .chart_of_account_long_term_foreign_agency_or_subsidiary_disbursed_receivable_parent_code(input.chart_of_account_long_term_foreign_agency_or_subsidiary_disbursed_receivable_parent_code.parse()?)
             .chart_of_account_long_term_non_domiciled_company_disbursed_receivable_parent_code(input.chart_of_account_long_term_non_domiciled_company_disbursed_receivable_parent_code.parse()?)
+
+            .chart_of_account_short_term_individual_interest_receivable_parent_code(input.chart_of_account_short_term_individual_interest_receivable_parent_code.parse()?)
+            .chart_of_account_short_term_government_entity_interest_receivable_parent_code(input.chart_of_account_short_term_government_entity_interest_receivable_parent_code.parse()?)
+            .chart_of_account_short_term_private_company_interest_receivable_parent_code(input.chart_of_account_short_term_private_company_interest_receivable_parent_code.parse()?)
+            .chart_of_account_short_term_bank_interest_receivable_parent_code(input.chart_of_account_short_term_bank_interest_receivable_parent_code.parse()?)
+            .chart_of_account_short_term_financial_institution_interest_receivable_parent_code(input.chart_of_account_short_term_financial_institution_interest_receivable_parent_code.parse()?)
+            .chart_of_account_long_term_foreign_agency_or_subsidiary_interest_receivable_parent_code(input.chart_of_account_long_term_foreign_agency_or_subsidiary_interest_receivable_parent_code.parse()?)
+            .chart_of_account_short_term_non_domiciled_company_interest_receivable_parent_code(input.chart_of_account_short_term_non_domiciled_company_interest_receivable_parent_code.parse()?)
+            .chart_of_account_long_term_individual_interest_receivable_parent_code(input.chart_of_account_long_term_individual_interest_receivable_parent_code.parse()?)
+            .chart_of_account_long_term_government_entity_interest_receivable_parent_code(input.chart_of_account_long_term_government_entity_interest_receivable_parent_code.parse()?)
+            .chart_of_account_long_term_private_company_interest_receivable_parent_code(input.chart_of_account_long_term_private_company_interest_receivable_parent_code.parse()?)
+            .chart_of_account_long_term_bank_interest_receivable_parent_code(input.chart_of_account_long_term_bank_interest_receivable_parent_code.parse()?)
+            .chart_of_account_long_term_financial_institution_interest_receivable_parent_code(input.chart_of_account_long_term_financial_institution_interest_receivable_parent_code.parse()?)
+            .chart_of_account_long_term_foreign_agency_or_subsidiary_interest_receivable_parent_code(input.chart_of_account_long_term_foreign_agency_or_subsidiary_interest_receivable_parent_code.parse()?)
+            .chart_of_account_long_term_non_domiciled_company_interest_receivable_parent_code(input.chart_of_account_long_term_non_domiciled_company_interest_receivable_parent_code.parse()?)
+
 
             .build()?;
         let config = app
