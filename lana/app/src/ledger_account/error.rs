d9a585aa0851ec2eba295caea599b87ac5b08134
Jiri Jakes <jiri@jirijakes.com>
2025-03-28 11:41:17 +0800

    Remove old ledger_account code

--- lana/app/src/ledger_account/error.rs
+++
@@ -1,9 +0,0 @@
-use thiserror::Error;
-
-#[derive(Error, Debug)]
-pub enum LedgerAccountError {
-    #[error("LedgerAccountError - AuthorizationError: {0}")]
-    AuthorizationError(#[from] authz::error::AuthorizationError),
-    #[error("LedgerAccountError - LedgerAccountLedgerError: {0}")]
-    LedgerAccountLedgerError(#[from] super::ledger::error::LedgerAccountLedgerError),
-}
