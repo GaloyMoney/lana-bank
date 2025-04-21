e94c52302aedbe1bad0cf4962a7218c9b24f55f4
Sandipan Dey <hey@sandipan.dev>
2025-04-21 18:50:37 +0530

    ci: retrigger on error

--- dev/bin/tilt-ci.sh
+++ dev/bin/tilt-ci.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 make reset-tf-state || true
+ps aux | grep "tilt up" | head -n 1 | cut -d" " -f2 | xargs kill -9 || true
 make dev-down || true
 
 cd dev
@@ -19,4 +20,14 @@ for i in {1..60}; do
 done
 
 tilt wait --for=condition=Ready --timeout=600s uiresource/core
+
+for i in {1..60}; do
+    if tilt get uiresource admin-panel -o json | jq -e '.status.runtimeStatus == "error"' > /dev/null; then
+        echo "uiresource/admin-panel is in error state. retrying..."
+        tilt trigger admin-panel
+        break
+    fi
+    sleep 1
+done
+
 tilt wait --for=condition=Ready --timeout=1200s uiresource/admin-panel
