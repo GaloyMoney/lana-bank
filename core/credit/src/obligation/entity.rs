189d4633020a41f1ee11c8bbd6a5318f71fe3463
vindard <17693119+vindard@users.noreply.github.com>
2025-04-22 23:18:31 -0400

    chore: add 'large_enum_variant' ignore

--- core/credit/src/obligation/entity.rs
+++ core/credit/src/obligation/entity.rs
@@ -70,6 +70,7 @@ impl ObligationsAmounts {
     }
 }
 
+#[allow(clippy::large_enum_variant)]
 #[derive(EsEvent, Debug, Clone, Serialize, Deserialize)]
 #[serde(tag = "type", rename_all = "snake_case")]
 #[es_event(id = "ObligationId")]
