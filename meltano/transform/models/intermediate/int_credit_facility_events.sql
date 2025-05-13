453ccd5c41c9e4f70fddf01badbd0e20695423dd
sebastienverreault <sv@galoy.io>
2025-05-13 16:52:56 +0900

    fix: remove redundant ctes, add account ids

--- meltano/transform/models/intermediate/int_credit_facility_events.sql
+++ meltano/transform/models/intermediate/int_credit_facility_events.sql
@@ -27,6 +27,19 @@ with initialized as (
         json_value(event, "$.terms.interest_overdue_duration.type") as interest_overdue_duration_type,
         cast(json_value(event, "$.terms.interest_overdue_duration.value") as integer) as interest_overdue_duration_value,
 
+        json_value(event, "$.account_ids.facility_account_id") as facility_account_id,
+        json_value(event, "$.account_ids.collateral_account_id") as collateral_account_id,
+        json_value(event, "$.account_ids.fee_income_account_id") as fee_income_account_id,
+        json_value(event, "$.account_ids.interest_income_account_id") as interest_income_account_id,
+        json_value(event, "$.account_ids.interest_defaulted_account_id") as interest_defaulted_account_id,
+        json_value(event, "$.account_ids.disbursed_defaulted_account_id") as disbursed_defaulted_account_id,
+        json_value(event, "$.account_ids.interest_receivable_due_account_id") as interest_receivable_due_account_id,
+        json_value(event, "$.account_ids.disbursed_receivable_due_account_id") as disbursed_receivable_due_account_id,
+        json_value(event, "$.account_ids.interest_receivable_overdue_account_id") as interest_receivable_overdue_account_id,
+        json_value(event, "$.account_ids.disbursed_receivable_overdue_account_id") as disbursed_receivable_overdue_account_id,
+        json_value(event, "$.account_ids.interest_receivable_not_yet_due_account_id") as interest_receivable_not_yet_due_account_id,
+        json_value(event, "$.account_ids.disbursed_receivable_not_yet_due_account_id") as disbursed_receivable_not_yet_due_account_id,
+
     from {{ ref('stg_credit_facility_events') }}
     where event_type = 'initialized'
 )
@@ -41,21 +54,6 @@ with initialized as (
     where event_type = "approval_process_concluded"
 )
 
-, collateral_state_changed as (
-    select
-        id as credit_facility_id,
-        max(recorded_at) as collateral_state_changed_recorded_at,
-        cast(json_value(any_value(event having max recorded_at), '$.collateral') as numeric) as collateral,
-        cast(json_value(any_value(event having max recorded_at), '$.price') as numeric) as price,
-        json_value(any_value(event having max recorded_at), '$.state') as state,
-        cast(json_value(any_value(event having max recorded_at), '$.outstanding.interest') as numeric) as outstanding_interest,
-        cast(json_value(any_value(event having max recorded_at), '$.outstanding.disbursed') as numeric) as outstanding_disbursed,
-
-    from {{ ref('stg_credit_facility_events') }}
-    where event_type = 'collateralization_state_changed'
-    group by credit_facility_id
-)
-
 , activated as (
     select
         id as credit_facility_id,
@@ -66,48 +64,13 @@ with initialized as (
     where event_type = "activated"
 )
 
-, accrual_cycle_started as (
-    select
-        id as credit_facility_id,
-        recorded_at as accrual_cycle_started_recorded_at,
-        cast(json_value(event, '$.idx') as integer) as idx,
-        json_value(event, '$.interest_accrual_id') as interest_accrual_id,
-        cast(json_value(event, '$.period.start') as timestamp) as period_start,
-        cast(json_value(event, '$.period.end') as timestamp) as period_end,
-        json_value(event, '$.period.interval.type') as period_interval_type,
-
-    from {{ ref('stg_credit_facility_events') }}
-    where event_type = "interest_accrual_cycle_started"
-)
-
-, accrual_cycle_concluded as (
-    select
-        id as credit_facility_id,
-        recorded_at as accrual_cycle_concluded_recorded_at,
-        cast(json_value(event, '$.idx') as integer) as idx,
-
-    from {{ ref('stg_credit_facility_events') }}
-    where event_type = "interest_accrual_cycle_concluded"
-)
-
-, accrual_cycles as (
-    select
-        credit_facility_id,
-        array_agg(struct(period_start as period_start, period_end as period_end, period_interval_type as period_interval_type, accrual_cycle_concluded_recorded_at is null as concluded)) as accrual_cycles
-    from accrual_cycle_started
-    left join accrual_cycle_concluded using (credit_facility_id, idx)
-    group by credit_facility_id
-)
-
 , final as (
     select
         *,
         case when duration_type = 'months' then timestamp_add(date(activated_at), interval duration_value month) end as maturity_at,
     from initialized
     left join concluded using (credit_facility_id)
-    left join collateral_state_changed using (credit_facility_id)
     left join activated using (credit_facility_id)
-    left join accrual_cycles using (credit_facility_id)
 )
 
 
