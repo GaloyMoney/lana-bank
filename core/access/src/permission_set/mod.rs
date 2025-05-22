3b4d0e12fee76925c002b25f6837268c56fa80f8
bodymindarts <justin@galoy.io>
2025-05-22 09:36:21 +0200

    fix: permission sets can be mutated

--- core/access/src/permission_set/mod.rs
+++ core/access/src/permission_set/mod.rs
@@ -4,8 +4,8 @@
 //!
 //! The main purpose of Permission Sets is to group related permissions under a common name and
 //! shield the administrator from actual permissions that can be too dynamic and have too high a granularity.
-//! Permission Sets are not intended to be created or deleted in a running application; they are expected
-//! to be created and defined during application bootstrap and remain unchanged for their entire life.
+//! Permission Sets are defined / created and modified during startup of the runtime. No mutating
+//! operations are exposed to the outside world.
 
 mod entity;
 mod error;
