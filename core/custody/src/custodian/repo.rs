b713595f36bc7b2be43f6e91e24c3ebfd0cd9a98
Vaibhav <thevaibhavdixit@gmail.com>
2025-06-11 16:52:20 +0530

    fix: use jsonb null type

--- core/custody/src/custodian/repo.rs
+++ core/custody/src/custodian/repo.rs
@@ -44,7 +44,7 @@ impl CustodianRepo {
         sqlx::query!(
             r#"
             UPDATE core_custodian_events
-            SET event = jsonb_set(event, '{encrypted_custodian_config}', 'null', false)
+            SET event = jsonb_set(event, '{encrypted_custodian_config}', 'null'::jsonb, false)
             WHERE id = $1 
               AND event_type = 'config_updated'
               AND event->'encrypted_custodian_config' IS NOT NULL;
