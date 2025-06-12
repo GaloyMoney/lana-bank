f881dd2f07d68276dd154f97cef9cea85a5bd354
sebastienverreault <sv@galoy.io>
2025-06-12 21:58:11 +0900

    update: stg as incremental & move else to intermed.

--- meltano/transform/models/intermediate/int_account_set_members.sql
+++ meltano/transform/models/intermediate/int_account_set_members.sql
@@ -4,7 +4,11 @@ select
     "Account" as member_type
 
 from {{ ref('stg_account_set_member_accounts') }}
-
+where _sdc_batched_at >= (
+    select coalesce(max(_sdc_batched_at), '1900-01-01')
+    from {{ ref('stg_core_chart_events') }}
+    where event_type = 'initialized'
+)
 union all
 
 select
@@ -13,3 +17,8 @@ select
     "AccountSet" as member_type
 
 from {{ ref('stg_account_set_member_account_sets') }}
+where _sdc_batched_at >= (
+    select coalesce(max(_sdc_batched_at), '1900-01-01')
+    from {{ ref('stg_core_chart_events') }}
+    where event_type = 'initialized'
+)
