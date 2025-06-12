a73ed2d66af5ebba1c524232242745d0dddd5e01
sebastienverreault <sv@galoy.io>
2025-06-12 20:18:48 +0900

    fix: use member account sets for accountset type

--- meltano/transform/models/intermediate/int_account_set_members.sql
+++ meltano/transform/models/intermediate/int_account_set_members.sql
@@ -9,7 +9,7 @@ union all
 
 select
     account_set_id,
-    member_account_id as member_id,
+    member_account_set_id as member_id,
     "AccountSet" as member_type
 
-from {{ ref('stg_account_set_member_accounts') }}
+from {{ ref('stg_account_set_member_account_sets') }}
