56789b25252c408cf98012bf36b7ea24c328b725
sebastienverreault <sv@galoy.io>
2025-06-18 12:30:00 +0900

    update: chart of account codification

--- meltano/transform/models/intermediate/int_core_chart_of_account_with_balances.sql
+++ meltano/transform/models/intermediate/int_core_chart_of_account_with_balances.sql
@@ -1,6 +1,6 @@
 with chart as (
     select
-        code,
+        dotted_code,
         name,
         account_set_id,
     from {{ ref('int_core_chart_of_accounts') }}
@@ -15,13 +15,13 @@ balances as (
 
 final as (
     select
-        c.code,
+        c.dotted_code,
         c.name,
         c.account_set_id,
         coalesce(sum(balance), 0) as balance
     from chart as c
     left join balances using (account_set_id)
-    group by code, name, account_set_id
+    group by dotted_code, name, account_set_id
 )
 
 select * from final
