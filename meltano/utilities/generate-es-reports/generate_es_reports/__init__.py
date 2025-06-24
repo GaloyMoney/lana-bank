c80ccebbd737e0a3b06819310eac4634430cbb3f
Jose Ignacio Rojas <jir@galoy.io>
2025-06-24 10:32:10 -0600

    chore(reporting): organize reports by norm in bucket
    As the number of reports increases, it will be good to keep them as organized as possible,
    this is a simple first stab at it.  Also provides a simple way to
    use a different serialization format depending on the norm.

--- meltano/utilities/generate-es-reports/generate_es_reports/__init__.py
+++ meltano/utilities/generate-es-reports/generate_es_reports/__init__.py
@@ -3,6 +3,10 @@ from datetime import datetime
 from google.cloud import bigquery, storage
 from dicttoxml import dicttoxml
 from google.oauth2 import service_account
+from re import compile
+
+table_name_pattern = compile(r"report_([a-z]+_\d+)_\d+_(.+)")
+
 
 def main():
     required_envs = [
@@ -25,30 +29,35 @@ def main():
             "GOOGLE_APPLICATION_CREDENTIALS environment variable must be set to the path of a valid service account JSON file."
         )
     credentials = service_account.Credentials.from_service_account_file(keyfile)
-
     bq_client = bigquery.Client(project=project_id, credentials=credentials)
 
-    # List all tables in the dataset and process those starting with "report_"
     tables_iter = bq_client.list_tables(dataset)
     for table in tables_iter:
-        if not table.table_id.startswith("report_"):
+        table_name = table.table_id
+        match = table_name_pattern.match(table_name)
+        if not match:
             continue
-        report_name = table.table_id
-        query = f"SELECT * FROM `{project_id}.{dataset}.{report_name}`;"
+        norm_name = match.group(1)
+        report_name = match.group(2)
+
+        query = f"SELECT * FROM `{project_id}.{dataset}.{table_name}`;"
         query_job = bq_client.query(query)
         rows = query_job.result()
         field_names = [field.name for field in rows.schema]
         rows_data = [{name: row[name] for name in field_names} for row in rows]
-        # Remove item_root/item_wrap, use only custom_root and attr_type
+
+        # TODO: Here we should branch to different serializations for different norms
+
         xml_bytes = dicttoxml(rows_data, custom_root="rows", attr_type=False)
         xml_content = xml_bytes.decode("utf-8")
         date_str = datetime.now().strftime("%Y-%m-%d")
-        blob_path = f"reports/{date_str}/{report_name}.xml"
+        blob_path = f"reports/{date_str}/{norm_name}/{report_name}.xml"
         storage_client = storage.Client(project=project_id, credentials=credentials)
         bucket = storage_client.bucket(bucket_name)
         blob = bucket.blob(blob_path)
         blob.upload_from_string(xml_content, content_type="text/xml")
         print(f"Uploaded XML report to gs://{bucket_name}/{blob_path}")
+
 
 if __name__ == "__main__":
     main()
