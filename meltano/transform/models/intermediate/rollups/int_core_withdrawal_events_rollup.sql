0524ec9835e2f2bcd2f1efd0a0870aaf3f38cf75
sebastienverreault <sv@galoy.io>
2025-07-01 17:39:16 +0900

    feat: withdrawal int view from stg

---
+++ meltano/transform/models/intermediate/rollups/int_core_withdrawal_events_rollup.sql
@@ -0,0 +1,33 @@
+with withdrawal as (
+    select
+        id as withdrawal_id,
+        deposit_account_id,
+
+        cast(amount as numeric) / {{ var('cents_per_usd') }} as amount_usd,
+        approved,
+        is_approval_process_concluded,
+        is_confirmed,
+        is_cancelled,
+
+        * except(
+            id,
+            deposit_account_id,
+            amount,
+            approved,
+            is_approval_process_concluded,
+            is_confirmed,
+            is_cancelled,
+
+            last_sequence,
+            _sdc_received_at,
+            _sdc_batched_at,
+            _sdc_extracted_at,
+            _sdc_deleted_at,
+            _sdc_sequence,
+            _sdc_table_version
+        )
+    from {{ ref('stg_core_withdrawal_events_rollup') }}
+)
+
+
+select * from withdrawal
