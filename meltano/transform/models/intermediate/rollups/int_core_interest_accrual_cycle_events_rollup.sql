4b7d658c45221b83d6514a6f4d8bcf348e607809
sebastienverreault <sv@galoy.io>
2025-07-02 16:15:28 +0900

    update: interest accrual cycle events rollup naming

--- meltano/transform/models/intermediate/rollups/int_core_interest_accrual_cycle_events_rollup.sql
+++ meltano/transform/models/intermediate/rollups/int_core_interest_accrual_cycle_events_rollup.sql
@@ -2,20 +2,12 @@ with interest_accrual_cycle as (
     select
         id as interest_accrual_cycle_id,
         credit_facility_id,
-        obligation_id,
         cast(facility_matures_at as timestamp) as facility_matures_at,
-        idx,
-        cast(effective as timestamp) as effective,
-        cast(accrued_at as timestamp) as accrued_at,
 
+        idx,
         cast(json_value(period, "$.start") as timestamp) as period_start_at,
         cast(json_value(period, "$.end") as timestamp) as period_end_at,
         json_value(period, "$.interval.type") as period_interval_type,
-
-
-        cast(amount as numeric) / {{ var('cents_per_usd') }} as amount_usd,
-        cast(total as numeric) / {{ var('cents_per_usd') }} as total_usd,
-        is_interest_accruals_posted,
 
         cast(json_value(terms, "$.annual_rate") as numeric) as annual_rate,
         cast(json_value(terms, "$.one_time_fee_rate") as numeric) as one_time_fee_rate,
@@ -32,6 +24,15 @@ with interest_accrual_cycle as (
         json_value(terms, "$.obligation_overdue_duration_from_due.type") as obligation_overdue_duration_from_due_type,
         cast(json_value(terms, "$.obligation_liquidation_duration_from_due.value") as integer) as obligation_liquidation_duration_from_due_value,
         json_value(terms, "$.obligation_liquidation_duration_from_due.type") as obligation_liquidation_duration_from_due_type,
+
+        cast(accrued_at as timestamp) as last_accrued_at,
+        cast(amount as numeric) / {{ var('cents_per_usd') }} as last_accrued_interest_amount_usd,
+
+        cast(effective as timestamp) as posted_effective,
+        cast(total as numeric) / {{ var('cents_per_usd') }} as posted_total_interest_usd,
+        obligation_id as posted_obligation_id,
+        is_interest_accruals_posted,
+
         created_at as interest_accrual_cycle_created_at,
         modified_at as interest_accrual_cycle_modified_at,
 
