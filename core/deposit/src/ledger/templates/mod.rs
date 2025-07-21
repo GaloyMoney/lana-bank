4fe30b0eda8676bf155e89a739366c05ab096cf9
Vaibhav <thevaibhavdixit@gmail.com>
2025-07-21 08:31:17 +0530

    feat: revert deposit logic

--- core/deposit/src/ledger/templates/mod.rs
+++ core/deposit/src/ledger/templates/mod.rs
@@ -1,10 +1,12 @@
 mod cancel_withdraw;
 mod confirm_withdraw;
+mod revert_deposit;
 mod initiate_withdraw;
 mod record_deposit;
 mod revert_withdraw;
 
 pub use cancel_withdraw::*;
+pub use revert_deposit::*: 
 pub use confirm_withdraw::*;
 pub use initiate_withdraw::*;
 pub use record_deposit::*;
