5a37fb12b303efa0a55cd1f28f701efa0d4724c8
Nicolas Burtey <nb@galoy.io>
2025-08-06 09:36:46 -0600

    fix: cargo fmt

--- lana/admin-server/src/graphql/credit_facility/mod.rs
+++ lana/admin-server/src/graphql/credit_facility/mod.rs
@@ -149,10 +149,7 @@ impl CreditFacility {
         Ok(process)
     }
 
-    async fn user_can_update_collateral(
-        &self,
-        ctx: &Context<'_>,
-    ) -> async_graphql::Result<bool> {
+    async fn user_can_update_collateral(&self, ctx: &Context<'_>) -> async_graphql::Result<bool> {
         let (app, sub) = crate::app_and_sub_from_ctx!(ctx);
         Ok(app
             .credit()
@@ -161,10 +158,7 @@ impl CreditFacility {
             .is_ok())
     }
 
-    async fn user_can_initiate_disbursal(
-        &self,
-        ctx: &Context<'_>,
-    ) -> async_graphql::Result<bool> {
+    async fn user_can_initiate_disbursal(&self, ctx: &Context<'_>) -> async_graphql::Result<bool> {
         let (app, sub) = crate::app_and_sub_from_ctx!(ctx);
         Ok(app
             .credit()
