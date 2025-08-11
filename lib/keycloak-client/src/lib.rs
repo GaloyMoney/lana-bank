5720dedf3aebbfd7ca5e91145dce5dde6f306c8f
Siddharth <siddharthtiwarikreplind@gmail.com>
2025-08-11 18:58:05 +0530

    chore: use keyclaok service account (#2568)
    * chore: use keyclaok service account
    
    * fix: check-code

--- lib/keycloak-client/src/lib.rs
+++ lib/keycloak-client/src/lib.rs
@@ -8,7 +8,7 @@ pub use config::KeycloakConnectionConfig;
 pub use error::KeycloakClientError;
 
 use keycloak::types::*;
-use keycloak::{KeycloakAdmin, KeycloakAdminToken};
+use keycloak::{KeycloakAdmin, KeycloakServiceAccountAdminTokenRetriever};
 use reqwest::Client;
 use uuid::Uuid;
 
@@ -16,33 +16,30 @@ use uuid::Uuid;
 pub struct KeycloakClient {
     connection: KeycloakConnectionConfig,
     http_client: Client,
-    realm: String,
 }
 
 impl KeycloakClient {
-    pub fn new(connection: KeycloakConnectionConfig, realm: String) -> Self {
+    pub fn new(connection: KeycloakConnectionConfig) -> Self {
         Self {
             connection,
             http_client: Client::new(),
-            realm,
         }
     }
 
-    async fn get_client(&self) -> Result<KeycloakAdmin, KeycloakClientError> {
-        let admin_token = KeycloakAdminToken::acquire(
-            &self.connection.url,
-            // should not use admin username and password, use client secret instead
-            // but it seems KeycloakAdmin plugin doesn't export this at the top level
-            // because maybe only at the "sub level" in generated code
-            // https://github.com/kilork/keycloak/tree/master/src/rest/generated_rest
-            &self.connection.admin_username,
-            &self.connection.admin_password,
-            &self.http_client,
-        )
-        .await?;
+    async fn get_client(
+        &self,
+    ) -> Result<KeycloakAdmin<KeycloakServiceAccountAdminTokenRetriever>, KeycloakClientError> {
+        let service_account_token_retriever =
+            KeycloakServiceAccountAdminTokenRetriever::create_with_custom_realm(
+                &self.connection.client_id,
+                &self.connection.client_secret,
+                &self.connection.realm,
+                self.http_client.clone(),
+            );
+
         Ok(KeycloakAdmin::new(
             &self.connection.url,
-            admin_token,
+            service_account_token_retriever,
             self.http_client.clone(),
         ))
     }
@@ -55,7 +52,9 @@ impl KeycloakClient {
             ..Default::default()
         };
         let client = self.get_client().await?;
-        let response = client.realm_users_post(&self.realm, user).await?;
+        let response = client
+            .realm_users_post(&self.connection.realm, user)
+            .await?;
         let user_id_str = response.to_id().ok_or_else(|| {
             KeycloakClientError::ParseError("User ID not found in response".to_string())
         })?;
@@ -75,7 +74,7 @@ impl KeycloakClient {
         };
         let client = self.get_client().await?;
         client
-            .realm_users_with_user_id_put(&self.realm, &user_id.to_string(), user)
+            .realm_users_with_user_id_put(&self.connection.realm, &user_id.to_string(), user)
             .await?;
         Ok(())
     }
@@ -83,7 +82,7 @@ impl KeycloakClient {
     pub async fn get_user(&self, user_id: Uuid) -> Result<UserRepresentation, KeycloakClientError> {
         let client = self.get_client().await?;
         let user = client
-            .realm_users_with_user_id_get(&self.realm, &user_id.to_string(), None)
+            .realm_users_with_user_id_get(&self.connection.realm, &user_id.to_string(), None)
             .await?;
         Ok(user)
     }
