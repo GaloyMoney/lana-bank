7bc502059a39857798624b0118f4a3817535d15b
Siddharth <siddharthtiwarikreplind@gmail.com>
2025-08-12 23:09:06 +0530

    chore: update error name KeycloakApiError to ApiError

--- lib/keycloak-client/src/error.rs
+++ lib/keycloak-client/src/error.rs
@@ -5,7 +5,7 @@ pub enum KeycloakClientError {
     #[error("KeycloakClientError - Parse error: {0}")]
     ParseError(String),
     #[error("KeycloakClientError - Keycloak API error: {0}")]
-    KeycloakApiError(#[from] keycloak::KeycloakError),
+    ApiError(#[from] keycloak::KeycloakError),
     #[error("KeycloakClientError - UUID parse error: {0}")]
     UuidError(#[from] uuid::Error),
     #[error("KeycloakClientError - User not found: {0}")]
