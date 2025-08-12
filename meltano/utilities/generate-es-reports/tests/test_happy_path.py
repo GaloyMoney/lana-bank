419f0146bff8be4bb526051a8076b96553fc0ef0
pablo <pablomartincalvo@gmail.com>
2025-08-12 12:45:37 +0200

    chore: add simple test

--- meltano/utilities/generate-es-reports/tests/test_happy_path.py
+++ meltano/utilities/generate-es-reports/tests/test_happy_path.py
@@ -1,10 +1,7 @@
-from pathlib import Path
-
 from xmlschema import XMLSchema
 
 
 from generate_es_reports.domain.report import (
-    ReportGeneratorConfig,
     ReportBatch,
     ReportJobDefinition,
     TXTFileOutputConfig,
@@ -14,7 +11,9 @@ from generate_es_reports.domain.report import (
 from generate_es_reports.io import MockTableFetcher, MockTable, LocalReportStorer
 
 
-def test_write_some_report():
+def test_write_some_report_produces_semantically_matching_outputs(
+    tmp_path,
+):  # tmp_path is a pytest managed dir that cleans up automagically
 
     xsd_string = """<?xml version="1.0" encoding="UTF-8"?>
         <xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema"
@@ -64,24 +63,24 @@ def test_write_some_report():
                     "order_id": "1",
                     "pizza": "pepperoni",
                     "quantity": "5",
-                    "created_at": "2025-01-01T00:00:00Z"
+                    "created_at": "2025-01-01T00:00:00Z",
                 },
                 {
                     "order_id": "2",
                     "pizza": "marinara",
                     "quantity": "1",
-                    "created_at": "2025-01-01T00:05:00Z"
+                    "created_at": "2025-01-01T00:05:00Z",
                 },
                 {
                     "order_id": "3",
                     "pizza": "diavola",
                     "quantity": "99",
-                    "created_at": "2025-01-01T00:10:00Z"
+                    "created_at": "2025-01-01T00:10:00Z",
                 },
-            )
+            ),
         )
     )
-    report_storer = LocalReportStorer(root_path=Path(__file__).parent)
+    report_storer = LocalReportStorer(root_path=tmp_path)
 
     report_batch = ReportBatch(
         run_id="test_run",
@@ -91,3 +90,14 @@ def test_write_some_report():
     )
 
     report_batch.generate_batch()
+
+    from_txt_str = open(
+        tmp_path / "reports" / "test_run" / "pizza_law" / "pizza_orders.txt"
+    ).read()
+    from_csv_str = open(
+        tmp_path / "reports" / "test_run" / "pizza_law" / "pizza_orders.csv"
+    ).read()
+
+    # If code runs fine until here, and the TXT and CSV versions of the output
+    # are the same, we kind of assume all went well.
+    assert from_csv_str == from_txt_str.replace("|", ",")
