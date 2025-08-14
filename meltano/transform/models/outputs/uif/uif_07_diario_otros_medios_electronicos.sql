a962c24f9268c033eab778d4b0f5a43f1509d49b
pablo <pablomartincalvo@gmail.com>
2025-08-14 12:24:19 +0200

    feat: add model with placeholders

---
+++ meltano/transform/models/outputs/uif/uif_07_diario_otros_medios_electronicos.sql
@@ -0,0 +1,15 @@
+select
+    null as numeroRegistroBancario,
+    null as estacionServicio,
+    null as fechaTransaccion,
+    null as detallesPersonaA,
+    null as numeroCuentaPO,
+    null as claseCuentaPO,
+    null as conceptoTransaccionPO,
+    null as valorOtrosMediosElectronicosPO,
+    null as numeroProductoPB,
+    null as claseCuentaPB,
+    null as montoTransaccionPB,
+    null as valorMedioElectronicoPB,
+    null as bancoCuentaDestinatariaPB
+
