23d8153e87d33fa1d831624906074f77df4bc1ce
Nicolas Burtey <nb@galoy.io>
2025-08-21 08:57:30 -0600

    chore: dag exporter for accounting

--- flake.nix
+++ flake.nix
@@ -325,6 +325,7 @@
           meltanoPkgs.meltano
           poppler_utils
           keycloak
+          graphviz
           # Font packages for PDF generation
           fontconfig
           dejavu_fonts # Provides serif, sans-serif, and monospace
