1dba416a666b842a8c385ff97058914f37b10e1f
pablo <pablomartincalvo@gmail.com>
2025-09-03 23:48:33 +0200

    chore: add dlt to deps

--- data-platform/dagster/src/lana-pipelines/definitions.py
+++ data-platform/dagster/src/lana-pipelines/definitions.py
@@ -1,14 +1,17 @@
 import dagster as dg
+import dlt
+
+def build_definitions():
+    @dg.asset(
+        op_tags={"operation": "example"},
+        partitions_def=dg.DailyPartitionsDefinition("2024-01-01"),
+    )
+    def example_asset(context: dg.AssetExecutionContext):
+        context.log.info(context.partition_key)
 
 
-@dg.asset(
-    op_tags={"operation": "example"},
-    partitions_def=dg.DailyPartitionsDefinition("2024-01-01"),
-)
-def example_asset(context: dg.AssetExecutionContext):
-    context.log.info(context.partition_key)
+    partitioned_asset_job = dg.define_asset_job("partitioned_job", selection=[example_asset])
 
-
-partitioned_asset_job = dg.define_asset_job("partitioned_job", selection=[example_asset])
-
-defs = dg.Definitions(assets=[example_asset], jobs=[partitioned_asset_job])
+    return dg.Definitions(assets=[example_asset], jobs=[partitioned_asset_job])
+     
+defs = build_definitions()
