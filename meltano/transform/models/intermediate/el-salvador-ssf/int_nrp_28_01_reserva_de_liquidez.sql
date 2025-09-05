3867d9b20be617557365b00ae9474d32fb486f33
sebastienverreault <sv@galoy.io>
2025-09-05 19:18:02 +0900

    fix: grouping in non-explain

--- meltano/transform/models/intermediate/el-salvador-ssf/int_nrp_28_01_reserva_de_liquidez.sql
+++ meltano/transform/models/intermediate/el-salvador-ssf/int_nrp_28_01_reserva_de_liquidez.sql
@@ -1,8 +1,14 @@
 with
 
 reserve as (
-    select *
+    select
+        order_by,
+        title,
+        sum(balance) as balance,
     from {{ ref('int_nrp_28_01_reserva_de_liquidez_explain') }}
+    group by
+        order_by,
+        title
 )
 
 select
