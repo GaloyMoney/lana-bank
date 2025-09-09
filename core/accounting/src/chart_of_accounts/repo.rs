216bf65913064ea35acf22bccba47f9cbc7331f5
Psycho-Pirate <prakharrsaxena@gmail.com>
2025-09-09 17:13:19 +0530

    refactor: implement ChartNode entity

--- core/accounting/src/chart_of_accounts/repo.rs
+++ core/accounting/src/chart_of_accounts/repo.rs
@@ -3,8 +3,20 @@ use sqlx::PgPool;
 use es_entity::*;
 
 use crate::primitives::ChartId;
+use crate::primitives::ChartNodeId;
 
 use super::{entity::*, error::ChartOfAccountsError};
+
+#[derive(EsRepo, Clone)]
+#[es_repo(
+    entity = "ChartNode",
+    err = "ChartOfAccountsError",
+    columns(chart_id(ty = "ChartId", update(persist = false), parent)),
+    tbl_prefix = "core"
+)]
+struct ChartNodeRepo {
+    pool: PgPool,
+}
 
 #[derive(EsRepo, Clone)]
 #[es_repo(
@@ -15,10 +27,16 @@ use super::{entity::*, error::ChartOfAccountsError};
 )]
 pub struct ChartRepo {
     pool: PgPool,
+
+    #[es_repo(nested)]
+    chart_nodes: ChartNodeRepo,
 }
 
 impl ChartRepo {
     pub fn new(pool: &PgPool) -> Self {
-        Self { pool: pool.clone() }
+        Self {
+            pool: pool.clone(),
+            chart_nodes: ChartNodeRepo { pool: pool.clone() },
+        }
     }
 }
