0732416aee71e261f4bef95a50379282ae56f4e4
pablo <pablomartincalvo@gmail.com>
2025-09-12 21:54:10 +0200

    chore: remove pointless example asset and job

--- data-platform/dagster/src/lana-pipelines/definitions.py
+++ data-platform/dagster/src/lana-pipelines/definitions.py
@@ -63,19 +63,11 @@ def build_definitions():
         context.log.info(f"Pipeline completed.")
         return load_info
 
-    @dg.asset(
-        op_tags={"operation": "example"},
-        partitions_def=dg.DailyPartitionsDefinition("2024-01-01"),
-    )
-    def example_asset(context: dg.AssetExecutionContext):
-        context.log.info(context.partition_key)
-
-    partitioned_asset_job = dg.define_asset_job("partitioned_job", selection=[example_asset])
     lana_pipeline_job = dg.define_asset_job("lana_pipeline_job", selection=[lana_pipeline_asset])
 
     return dg.Definitions(
-        assets=[example_asset, lana_pipeline_asset], 
-        jobs=[partitioned_asset_job, lana_pipeline_job]
+        assets=[lana_pipeline_asset], 
+        jobs=[lana_pipeline_job]
     )
      
 defs = build_definitions()
