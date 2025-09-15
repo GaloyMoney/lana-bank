671214ef76bd347ac20d9edc54900ec020da869b
Lakshyyaa <lakshh113@gmail.com>
2025-09-15 18:02:48 +0530

    chore: generate types

--- apps/admin-panel/lib/graphql/generated/index.ts
+++ apps/admin-panel/lib/graphql/generated/index.ts
@@ -303,13 +303,6 @@ export type ChartOfAccountsCsvImportPayload = {
   chartOfAccounts: ChartOfAccounts;
 };
 
-export type Collateral = {
-  __typename?: 'Collateral';
-  collateralId: Scalars['UUID']['output'];
-  id: Scalars['ID']['output'];
-  walletId?: Maybe<Scalars['UUID']['output']>;
-};
-
 export enum CollateralAction {
   Add = 'ADD',
   Remove = 'REMOVE'
@@ -1406,7 +1399,7 @@ export type LedgerTransactionEdge = {
   node: LedgerTransaction;
 };
 
-export type LedgerTransactionEntity = Collateral | CreditFacilityDisbursal | Deposit | Withdrawal;
+export type LedgerTransactionEntity = CreditFacilityDisbursal | Deposit | Withdrawal;
 
 export type Loan = {
   __typename?: 'Loan';
