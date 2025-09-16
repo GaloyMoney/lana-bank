85d3a8149e5eac451f88ebbeaa66ef83bbad6619
pablo <pablomartincalvo@gmail.com>
2025-09-16 10:26:31 +0200

    chore: refactor resource into module

---
+++ data-platform/dagster/src/lana_pipelines/resources.py
@@ -0,0 +1,11 @@
+from dlt.sources.sql_database import sql_table
+
+def create_postgres_resource(connection_string_details, table_name):       
+        postgres_resource = sql_table(
+            credentials=connection_string_details,
+            schema="public",
+            backend="sqlalchemy",
+            table=table_name
+        )
+
+        return postgres_resource
