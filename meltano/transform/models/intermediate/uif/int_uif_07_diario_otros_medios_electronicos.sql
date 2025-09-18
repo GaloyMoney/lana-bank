a7ac21a7c4c9887363f7dba0fab197f10539cca7
pablo <pablomartincalvo@gmail.com>
2025-09-18 12:42:35 +0200

    chore: with. and dep. as matching interface CTEs

--- meltano/transform/models/intermediate/uif/int_uif_07_diario_otros_medios_electronicos.sql
+++ meltano/transform/models/intermediate/uif/int_uif_07_diario_otros_medios_electronicos.sql
@@ -1,7 +1,7 @@
 with seed_bank_address as (select * from {{ ref("seed_bank_address") }}),
-int_core_withdrawal_events_rollup_sequence as (select * from {{ ref("int_core_withdrawal_events_rollup_sequence") }}) ,
+int_core_withdrawal_events_rollup_sequence as (select * from {{ ref("int_core_withdrawal_events_rollup_sequence") }}),
 int_core_withdrawal_events_rollup as (select * from {{ ref("int_core_withdrawal_events_rollup") }}),
-int_core_deposit_events_rollup_sequence as (select * from {{ ref("int_core_deposit_events_rollup_sequence") }}) ,
+int_core_deposit_events_rollup_sequence as (select * from {{ ref("int_core_deposit_events_rollup_sequence") }}),
 int_core_deposit_events_rollup as (select * from {{ ref("int_core_deposit_events_rollup") }}),
 int_core_deposit_account_events_rollup as (select * from {{ ref("int_core_deposit_account_events_rollup") }}),
 confirmed_withdrawals as (
@@ -29,14 +29,14 @@ confirmed_deposits as (
 deposit_confirmation_timestamps as (
     select 
         ers.deposit_id,
-        min(ers.deposit_modified_at)
+        min(ers.deposit_modified_at) as deposit_confirmed_at
     from int_core_deposit_events_rollup_sequence ers
     inner join confirmed_deposits cd
         on ers.deposit_id = cd.deposit_id
     where status = 'Confirmed'
     group by deposit_id
-)
-
+),
+withdrawal_transactions as (
 select
     wer.withdrawal_id as numeroRegistroBancario, -- probably this should be a public id and not the private uuid
     JSON_OBJECT(
@@ -67,3 +67,36 @@ left join int_core_deposit_account_events_rollup aer
     on wer.deposit_account_id = aer.deposit_account_id
 cross join -- Note: this assumes there's only one address!
 seed_bank_address as bank_address
+),
+deposit_transactions as (
+select
+    der.deposit_id as numeroRegistroBancario, -- probably this should be a public id and not the private uuid
+    JSON_OBJECT(
+        'direccionAgencia', bank_address.full_address,
+        'idDepartamento', bank_address.region_id,
+        'idMunicipio', bank_address.town_id
+    ) as estacionServicio,
+    dct.deposit_confirmed_at as fechaTransaccion,
+    null as tipoPersonaA,
+    null as detallesPersonaA,
+    null as tipoPersonaB,
+    null as detallesPersonaB,
+    null as numeroCuentaPO,
+    null as claseCuentaPO,
+    null as conceptoTransaccionPO,
+    der.amount_usd as valorOtrosMediosElectronicosPO,
+    aer.public_id as numeroProductoPB,
+    "Cuenta Corriente" as claseCuentaPB,
+    der.amount_usd as montoTransaccionPB,
+    der.amount_usd as valorMedioElectronicoPB,
+    null as bancoCuentaDestinatariaPB
+from int_core_deposit_events_rollup der
+inner join confirmed_deposits cd 
+    on der.deposit_id = cd.deposit_id
+left join deposit_confirmation_timestamps dct
+    on der.deposit_id = dct.deposit_id
+left join int_core_deposit_account_events_rollup aer
+    on der.deposit_account_id = aer.deposit_account_id
+cross join -- Note: this assumes there's only one address!
+seed_bank_address as bank_address
+)
