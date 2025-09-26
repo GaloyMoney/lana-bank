98b45ca71bb6347a7219f3c1608ed9ca42eba3ac
pablo <pablomartincalvo@gmail.com>
2025-09-26 13:54:43 +0200

    feat: sensor working

--- data-platform/dagster/src/lana_pipelines/definitions.py
+++ data-platform/dagster/src/lana_pipelines/definitions.py
@@ -1,7 +1,9 @@
+import datetime
+
 import dagster as dg
 
 from lana_pipelines.assets import build_lana_source_asset, build_lana_to_dw_el_asset, build_dbt_assets, build_generate_es_report_asset
-from lana_pipelines.resources import dbt_resource
+from lana_pipelines.resources import dbt_resource, poll_max_value_in_table_col
 
 
 def build_definitions():
@@ -58,39 +60,86 @@ def build_definitions():
         "dbt": dbt_resource
     }
 
+    all_schedules = []
+    all_sensors = []
 
-    flana_to_dw_el_job_schedule = dg.ScheduleDefinition(
-        name="lana_to_dw_el_job_schedule",
-        cron_schedule="* * * * *",
-        job=lana_to_dw_el_job,
-        default_status=dg.DefaultScheduleStatus.RUNNING
-    )
+    if dg.EnvVar("AUTOMATION_STYLE").get_value() == "scheduled":
 
-    build_dbt_job_schedule = dg.ScheduleDefinition(
-        name="build_dbt_job_schedule",
-        cron_schedule="*/2 * * * *",
-        job=build_dbt_job,
-        default_status=dg.DefaultScheduleStatus.RUNNING
-    )
+        flana_to_dw_el_job_schedule = dg.ScheduleDefinition(
+            name="lana_to_dw_el_job_schedule",
+            cron_schedule="* * * * *",
+            job=lana_to_dw_el_job,
+            default_status=dg.DefaultScheduleStatus.RUNNING
+        )
 
-    build_generate_es_report_job_schedule = dg.ScheduleDefinition(
-        name="build_generate_es_report_job_schedule",
-        cron_schedule="*/3 * * * *",
-        job=build_generate_es_report_job,
-        default_status=dg.DefaultScheduleStatus.RUNNING
-    )
+        build_dbt_job_schedule = dg.ScheduleDefinition(
+            name="build_dbt_job_schedule",
+            cron_schedule="*/2 * * * *",
+            job=build_dbt_job,
+            default_status=dg.DefaultScheduleStatus.RUNNING
+        )
 
-    all_schedules = [
-        flana_to_dw_el_job_schedule,
-        build_dbt_job_schedule,
-        build_generate_es_report_job_schedule
-    ]
+        build_generate_es_report_job_schedule = dg.ScheduleDefinition(
+            name="build_generate_es_report_job_schedule",
+            cron_schedule="*/3 * * * *",
+            job=build_generate_es_report_job,
+            default_status=dg.DefaultScheduleStatus.RUNNING
+        )
+
+        all_schedules = [
+            flana_to_dw_el_job_schedule,
+            build_dbt_job_schedule,
+            build_generate_es_report_job_schedule
+        ]
+
+    if dg.EnvVar("AUTOMATION_STYLE").get_value() == "mixed":
+
+        @dg.sensor(
+            job=lana_to_dw_el_job,
+            minimum_interval_seconds=10,
+            default_status=dg.DefaultSensorStatus.RUNNING,
+        )
+        def lana_el_sensor(context):
+            connection_string = "postgresql://user:password@172.17.0.1:5433/pg"
+
+            current_highest_timestamp = poll_max_value_in_table_col(
+                connection_string_details=connection_string,
+                table_name="core_deposit_events_rollup",
+                fieldname="created_at",
+                
+            )
+
+            highest_seen_timestamp = datetime.datetime.fromisoformat(context.cursor) if context.cursor else datetime.datetime(1970, 1, 1, 0, 0, 0, tzinfo=datetime.timezone.utc)
+
+            context.log.info(f"Highest timestamp seen in Dagster: {highest_seen_timestamp}.")
+            context.log.info(f"Current highest timestamp in source: {current_highest_timestamp}.")
+
+            if current_highest_timestamp > highest_seen_timestamp:
+                    yield dg.RunRequest(run_key=str(current_highest_timestamp))
+            else:
+                yield dg.SkipReason("No new files found")
+            
+            context.update_cursor(current_highest_timestamp.isoformat())
+
+        all_sensors = [lana_el_sensor]
+        
+
+        build_generate_es_report_job_schedule = dg.ScheduleDefinition(
+            name="build_generate_es_report_job_schedule",
+            cron_schedule="*/3 * * * *",
+            job=build_generate_es_report_job,
+            default_status=dg.DefaultScheduleStatus.RUNNING
+        )
+
+        all_schedules = [build_generate_es_report_job_schedule]
+
 
     return dg.Definitions(
         assets=all_assets,
         jobs=all_jobs, 
         resources=all_resources,
-        schedules=all_schedules
+        schedules=all_schedules,
+        sensors=all_sensors
     )
 
 
--- data-platform/dagster/src/lana_pipelines/resources.py
+++ data-platform/dagster/src/lana_pipelines/resources.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 from dagster_dbt import DbtCliResource
 
-from dlt.sources.sql_database import sql_table
+from dlt.sources.sql_database import sql_table, sql_database
 
 def create_postgres_resource(connection_string_details, table_name):
     postgres_resource = sql_table(
@@ -13,6 +13,18 @@ def create_postgres_resource(connection_string_details, table_name):
     )
 
     return postgres_resource
+
+
+def poll_max_value_in_table_col(connection_string_details, table_name, fieldname):
+    
+    import psycopg2
+
+    conn = psycopg2.connect(connection_string_details)
+    with conn, conn.cursor() as cur:
+        cur.execute(f"SELECT MAX({fieldname}) FROM {table_name}")
+        max_created_at = cur.fetchone()[0]
+    
+    return max_created_at
 
 dbt_resource = DbtCliResource(project_dir=Path("dbt_lana_dw/"))
 dbt_parse_invocation = dbt_resource.cli(["parse"], manifest={}).wait()
