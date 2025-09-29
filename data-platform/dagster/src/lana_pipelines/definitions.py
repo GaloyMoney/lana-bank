ddc951afa4c8880127c7f6d8a2eabe7eaca75b42
pablo <pablomartincalvo@gmail.com>
2025-09-29 22:43:55 +0200

    refactor: extract sensors

--- data-platform/dagster/src/lana_pipelines/definitions.py
+++ data-platform/dagster/src/lana_pipelines/definitions.py
@@ -9,6 +9,7 @@ from lana_pipelines.assets import (
     build_generate_es_report_asset,
 )
 from lana_pipelines.resources import BigQueryResource, PostgresResource, dbt_resource
+from lana_pipelines.sensors import build_lana_el_sensor
 from lana_pipelines import constants
 
 
@@ -69,40 +70,7 @@ class DefinitionBuilder:
             self.schedule_definitions.append(lana_to_dw_el_job_schedule)
 
         if self.automation_style == "mixed":
-
-            @dg.sensor(
-                job=lana_to_dw_el_job,
-                minimum_interval_seconds=10,
-                default_status=dg.DefaultSensorStatus.RUNNING,
-            )
-            def lana_el_sensor(context, lana_core_pg: PostgresResource):
-                current_highest_timestamp = lana_core_pg.poll_max_value_in_table_col(
-                    table_name="core_deposit_events_rollup",
-                    fieldname="created_at",
-                )
-
-                highest_seen_timestamp = (
-                    datetime.datetime.fromisoformat(context.cursor)
-                    if context.cursor
-                    else datetime.datetime(
-                        1970, 1, 1, 0, 0, 0, tzinfo=datetime.timezone.utc
-                    )
-                )
-
-                context.log.info(
-                    f"Highest timestamp seen in Dagster: {highest_seen_timestamp}."
-                )
-                context.log.info(
-                    f"Current highest timestamp in source: {current_highest_timestamp}."
-                )
-
-                if current_highest_timestamp > highest_seen_timestamp:
-                    yield dg.RunRequest(run_key=str(current_highest_timestamp))
-                else:
-                    yield dg.SkipReason("No new files found")
-
-                context.update_cursor(current_highest_timestamp.isoformat())
-
+            lana_el_sensor = build_lana_el_sensor(lana_to_dw_el_job)
             self.sensor_definitions.append(lana_el_sensor)
 
     def build_dbt_layer(self):
