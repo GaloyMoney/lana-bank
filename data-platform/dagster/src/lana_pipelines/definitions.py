91d8ad794b86b074d3cc99e7482568ce247dc181
pablo <pablomartincalvo@gmail.com>
2025-09-29 17:17:33 +0200

    refactor: shaping building process

--- data-platform/dagster/src/lana_pipelines/definitions.py
+++ data-platform/dagster/src/lana_pipelines/definitions.py
@@ -6,32 +6,36 @@ from lana_pipelines.assets import build_lana_source_asset, build_lana_to_dw_el_a
 from lana_pipelines.resources import dbt_resource, poll_max_value_in_table_col
 
 
-def build_definitions():
+class DefinitionBuilder():
+     
+    def __init__(self, automation_style = "no_automation"):
 
-    lana_to_dw_el_tables = (
-        "core_deposit_account_events_rollup",
-        "core_deposit_events_rollup",
-        "core_withdrawal_events_rollup",
-        "core_public_ids",
-    )
+        self.automation_style = automation_style
 
-    lana_source_assets = [
-        build_lana_source_asset(table_name=table_name)
-        for table_name in lana_to_dw_el_tables
-    ]
+        self.asset_definitions = []
+        self.job_definitions = []
+        self.resource_definitions = []
+        self.schedule_definitions = []
+        self.sensor_definitions = []
 
-    lana_to_dw_el_assets = [
-        build_lana_to_dw_el_asset(table_name=table_name)
-        for table_name in lana_to_dw_el_tables
-    ]
+        self.EL_TABLES = (
+            "core_deposit_account_events_rollup",
+            "core_deposit_events_rollup",
+            "core_withdrawal_events_rollup",
+            "core_public_ids",
+        )
+    
+    def build_lana_source_layer(self):
+        assets = [build_lana_source_asset(table_name=table_name) for table_name in self.EL_TABLES]
+        self.asset_definitions.extend(assets)
 
-    dbt_assets = [build_dbt_assets()]
-
-    generate_es_report_asset = [build_generate_es_report_asset()]
-
-    lana_to_dw_el_job = dg.define_asset_job(
+    def build_lana_to_dw_el_layer(self):
+        assets = [build_lana_to_dw_el_asset(table_name=table_name) for table_name in self.EL_TABLES]
+        self.asset_definitions.extend(assets)
+        
+        lana_to_dw_el_job = dg.define_asset_job(
         name="lana_to_dw_el_job",
-        selection=lana_to_dw_el_assets,
+        selection=assets,
         # Below is a silly hardcode to prevent running into rate limiting with
         # BQ: we should research how to avoid it because it gets triggered 
         # with rather low volumes.
@@ -42,7 +46,67 @@ def build_definitions():
                 }
             }
         },
+        )
+
+        self.job_definitions.append(lana_to_dw_el_job)
+
+        if self.automation_style == "scheduled":
+            lana_to_dw_el_job_schedule = dg.ScheduleDefinition(
+                name="lana_to_dw_el_job_schedule",
+                cron_schedule="* * * * *",
+                job=lana_to_dw_el_job,
+                default_status=dg.DefaultScheduleStatus.RUNNING
+            )
+
+            self.schedule_definitions.append(lana_to_dw_el_job_schedule)
+        
+        if self.automation_style == "mixed":
+            @dg.sensor(
+            job=lana_to_dw_el_job,
+            minimum_interval_seconds=10,
+            default_status=dg.DefaultSensorStatus.RUNNING,
+            )
+            def lana_el_sensor(context):
+                connection_string = "postgresql://user:password@172.17.0.1:5433/pg"
+
+                current_highest_timestamp = poll_max_value_in_table_col(
+                    connection_string_details=connection_string,
+                    table_name="core_deposit_events_rollup",
+                    fieldname="created_at",
+                    
+                )
+
+                highest_seen_timestamp = datetime.datetime.fromisoformat(context.cursor) if context.cursor else datetime.datetime(1970, 1, 1, 0, 0, 0, tzinfo=datetime.timezone.utc)
+
+                context.log.info(f"Highest timestamp seen in Dagster: {highest_seen_timestamp}.")
+                context.log.info(f"Current highest timestamp in source: {current_highest_timestamp}.")
+
+                if current_highest_timestamp > highest_seen_timestamp:
+                        yield dg.RunRequest(run_key=str(current_highest_timestamp))
+                else:
+                    yield dg.SkipReason("No new files found")
+                
+                context.update_cursor(current_highest_timestamp.isoformat())
+
+            self.sensor_definitions.append(lana_el_sensor)
+         
+
+             
+
+def build_definitions():
+
+    definition_builder = DefinitionBuilder(
+        automation_style=dg.EnvVar("AUTOMATION_STYLE").get_value()
     )
+
+    definition_builder.build_lana_source_layer()
+    definition_builder.build_lana_to_dw_el_layer()
+
+    dbt_assets = [build_dbt_assets()]
+
+    generate_es_report_asset = [build_generate_es_report_asset()]
+
+    
 
     build_dbt_job = dg.define_asset_job(
         name="build_dbt_job",
@@ -59,8 +123,8 @@ def build_definitions():
         selection="seed_bank_address",
     )
 
-    all_assets = lana_source_assets + lana_to_dw_el_assets + dbt_assets + generate_es_report_asset
-    all_jobs = [lana_to_dw_el_job, build_dbt_job, build_generate_es_report_job, build_seed_bank_address_job]
+    all_assets = definition_builder.asset_definitions + dbt_assets + generate_es_report_asset
+    all_jobs = definition_builder.job_definitions + [build_dbt_job, build_generate_es_report_job, build_seed_bank_address_job]
     all_resources = {
         "dbt": dbt_resource
     }
@@ -70,12 +134,7 @@ def build_definitions():
 
     if dg.EnvVar("AUTOMATION_STYLE").get_value() == "scheduled":
 
-        flana_to_dw_el_job_schedule = dg.ScheduleDefinition(
-            name="lana_to_dw_el_job_schedule",
-            cron_schedule="* * * * *",
-            job=lana_to_dw_el_job,
-            default_status=dg.DefaultScheduleStatus.RUNNING
-        )
+        
 
         build_dbt_job_schedule = dg.ScheduleDefinition(
             name="build_dbt_job_schedule",
@@ -98,43 +157,14 @@ def build_definitions():
             default_status=dg.DefaultScheduleStatus.RUNNING
         )
 
-        all_schedules = [
-            flana_to_dw_el_job_schedule,
+        all_schedules = definition_builder.schedule_definitions + [
             build_dbt_job_schedule,
             build_seed_bank_address_job_schedule,
             build_generate_es_report_job_schedule
         ]
 
     if dg.EnvVar("AUTOMATION_STYLE").get_value() == "mixed":
-
-        @dg.sensor(
-            job=lana_to_dw_el_job,
-            minimum_interval_seconds=10,
-            default_status=dg.DefaultSensorStatus.RUNNING,
-        )
-        def lana_el_sensor(context):
-            connection_string = "postgresql://user:password@172.17.0.1:5433/pg"
-
-            current_highest_timestamp = poll_max_value_in_table_col(
-                connection_string_details=connection_string,
-                table_name="core_deposit_events_rollup",
-                fieldname="created_at",
-                
-            )
-
-            highest_seen_timestamp = datetime.datetime.fromisoformat(context.cursor) if context.cursor else datetime.datetime(1970, 1, 1, 0, 0, 0, tzinfo=datetime.timezone.utc)
-
-            context.log.info(f"Highest timestamp seen in Dagster: {highest_seen_timestamp}.")
-            context.log.info(f"Current highest timestamp in source: {current_highest_timestamp}.")
-
-            if current_highest_timestamp > highest_seen_timestamp:
-                    yield dg.RunRequest(run_key=str(current_highest_timestamp))
-            else:
-                yield dg.SkipReason("No new files found")
-            
-            context.update_cursor(current_highest_timestamp.isoformat())
-
-        all_sensors = [lana_el_sensor]
+        all_sensors = definition_builder.sensor_definitions
         
         build_generate_es_report_job_schedule = dg.ScheduleDefinition(
             name="build_generate_es_report_job_schedule",
@@ -150,7 +180,7 @@ def build_definitions():
             default_status=dg.DefaultScheduleStatus.RUNNING
         )
 
-        all_schedules = [build_generate_es_report_job_schedule, build_seed_bank_address_job_schedule]
+        all_schedules = definition_builder.schedule_definitions + [build_generate_es_report_job_schedule, build_seed_bank_address_job_schedule]
 
     if dg.EnvVar("AUTOMATION_STYLE").get_value() == "no_automation":
          # We do nothing and let all_schedules and all_sensors go in empty
