feda3f72461785c6f233bc413396751fd8c0be2b
Jiri Jakes <jiri@jirijakes.com>
2025-10-14 10:26:52 -0300

    feat: Introduce Accounting Period entity

---
+++ core/accounting/src/accounting_period/error.rs
@@ -0,0 +1,15 @@
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum AccountingPeriodError {
+    #[error("AccountingPeriodError - Sqlx: {0}")]
+    Sqlx(#[from] sqlx::Error),
+    #[error("AccountingPeriodError - EsEntityError: {0}")]
+    EsEntityError(es_entity::EsEntityError),
+    #[error("AccountingPeriodError - CursorDestructureError: {0}")]
+    CursorDestructureError(#[from] es_entity::CursorDestructureError),
+    #[error("AccountingPeriodError - NoOpenAccountingPeriodFound")]
+    NoOpenAccountingPeriodFound,
+}
+
+es_entity::from_es_entity_error!(AccountingPeriodError);
