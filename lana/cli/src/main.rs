e0c961a476aca6994715b6ae8bcc13fe07f28e69
Vaibhav <thevaibhavdixit@gmail.com>
2025-10-15 11:22:11 +0530

    chore: do not modify main.rs

--- lana/cli/src/main.rs
+++ lana/cli/src/main.rs
@@ -1,14 +1,7 @@
 #![cfg_attr(feature = "fail-on-warnings", deny(warnings))]
 #![cfg_attr(feature = "fail-on-warnings", deny(clippy::all))]
 
-use tokio::runtime::Builder;
-
-const THREAD_STACK_SIZE: usize = 4 * 1024 * 1024; // 4 MiB
-
-fn main() -> anyhow::Result<()> {
-    let runtime = Builder::new_multi_thread()
-        .enable_all()
-        .thread_stack_size(THREAD_STACK_SIZE)
-        .build()?;
-    runtime.block_on(async { lana_cli::run().await })
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    lana_cli::run().await
 }
