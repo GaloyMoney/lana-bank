d85e8385ee88ff44b47fc7a59aadb77113bd66f6
Sandipan <hey@sandipan.dev>
2025-10-15 23:13:20 +0530

    feat: annual closing chart of account config (#2866)
    * chore: chart of accounts integration config passed through to module
    
    * chore: perms work
    
    * chore: chart passed around
    
    * chore: update the config
    
    * chore: attachment of the integration meta

--- core/accounting/src/annual_closing_transaction/ledger/mod.rs
+++ core/accounting/src/annual_closing_transaction/ledger/mod.rs
@@ -1,12 +1,14 @@
 mod template;
 
+use audit::AuditInfo;
+use serde::{Deserialize, Serialize};
 use template::*;
 pub use template::{AnnualClosingTransactionParams, EntryParams};
 
 use crate::primitives::CalaTxId;
-use cala_ledger::CalaLedger;
+use cala_ledger::{AccountSetId, CalaLedger, account_set::AccountSetUpdate};
 
-use super::error::AnnualClosingTransactionError;
+use super::{ChartOfAccountsIntegrationConfig, error::AnnualClosingTransactionError};
 
 #[derive(Clone)]
 pub struct AnnualClosingTransactionLedger {
@@ -14,6 +16,8 @@ pub struct AnnualClosingTransactionLedger {
 }
 
 impl AnnualClosingTransactionLedger {
+    pub const CHART_OF_ACCOUNTS_INTEGRATION_KEY: &'static str = "chart_of_accounts_integration";
+
     pub fn new(cala: &CalaLedger) -> Self {
         Self { cala: cala.clone() }
     }
@@ -39,4 +43,88 @@ impl AnnualClosingTransactionLedger {
 
         Ok(())
     }
+
+    pub async fn get_chart_of_accounts_integration_config(
+        &self,
+        root_chart_account_set_id: impl Into<AccountSetId>,
+    ) -> Result<Option<ChartOfAccountsIntegrationConfig>, AnnualClosingTransactionError> {
+        let root_chart_account_set_id = root_chart_account_set_id.into();
+        let account_set = self
+            .cala
+            .account_sets()
+            .find(root_chart_account_set_id)
+            .await?;
+        if let Some(meta) = account_set.values().metadata.as_ref() {
+            if let Some(chart_of_accounts_integration) =
+                meta.get(Self::CHART_OF_ACCOUNTS_INTEGRATION_KEY)
+            {
+                let meta: ChartOfAccountsIntegrationMeta =
+                    serde_json::from_value(chart_of_accounts_integration.clone())
+                        .expect("could not deserialize chart_of_accounts_integration meta");
+                Ok(Some(meta.config))
+            } else {
+                Ok(None)
+            }
+        } else {
+            Ok(None)
+        }
+    }
+
+    pub async fn attach_chart_of_accounts_integration_meta(
+        &self,
+        op: es_entity::DbOp<'_>,
+        root_chart_account_set_id: impl Into<AccountSetId>,
+        config: ChartOfAccountsIntegrationMeta,
+    ) -> Result<(), AnnualClosingTransactionError> {
+        let root_chart_account_set_id = root_chart_account_set_id.into();
+        let mut account_set = self
+            .cala
+            .account_sets()
+            .find(root_chart_account_set_id)
+            .await?;
+
+        let mut metadata = account_set
+            .values()
+            .metadata
+            .clone()
+            .unwrap_or_else(|| serde_json::json!({}));
+
+        metadata
+            .as_object_mut()
+            .expect("metadata should be an object")
+            .insert(
+                Self::CHART_OF_ACCOUNTS_INTEGRATION_KEY.to_string(),
+                serde_json::to_value(config)
+                    .expect("could not serialize chart_of_accounts_integration meta"),
+            );
+
+        let mut update_values = AccountSetUpdate::default();
+        update_values
+            .metadata(Some(metadata))
+            .expect("failed to serialize metadata");
+        account_set.update(update_values);
+
+        let mut op = self
+            .cala
+            .ledger_operation_from_db_op(op.with_db_time().await?);
+        self.cala
+            .account_sets()
+            .persist_in_op(&mut op, &mut account_set)
+            .await?;
+
+        op.commit().await?;
+        Ok(())
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct ChartOfAccountsIntegrationMeta {
+    pub config: ChartOfAccountsIntegrationConfig,
+    pub audit_info: AuditInfo,
+
+    pub revenue_child_account_set_id_from_chart: AccountSetId,
+    pub cost_of_revenue_child_account_set_id_from_chart: AccountSetId,
+    pub expenses_child_account_set_id_from_chart: AccountSetId,
+    pub equity_retained_earnings_child_account_set_id_from_chart: AccountSetId,
+    pub equity_retained_losses_child_account_set_id_from_chart: AccountSetId,
 }
--- lana/app/src/accounting_init/seed/module_config/mod.rs
+++ lana/app/src/accounting_init/seed/module_config/mod.rs
@@ -1,3 +1,4 @@
+pub(super) mod annual_closing_transaction;
 pub(super) mod balance_sheet;
 pub(super) mod credit;
 pub(super) mod deposit;
