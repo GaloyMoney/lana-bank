da4ae557407408fd6c27d12d89650c71495e1c56
pablo <pablomartincalvo@gmail.com>
2025-10-20 12:57:00 +0200

    ci: add dagster to image bumping in private charts

--- ci/tasks/bump-image-digest.sh
+++ ci/tasks/bump-image-digest.sh
@@ -5,6 +5,7 @@ export digest=$(cat ./latest-image/digest)
 export admin_panel_image_digest=$(cat ./admin-panel-latest-image/digest)
 export customer_portal_image_digest=$(cat ./customer-portal-latest-image/digest)
 export meltano_image_digest=$(cat ./meltano-latest-image/digest)
+export dagster_image_digest=$(cat ./dagster-latest-image/digest)
 
 
 export ref=$(cat ./repo/.git/short_ref)
@@ -16,6 +17,7 @@ yq -i e '.lanaBank.image.digest = strenv(digest)' ./charts/${CHARTS_SUBDIR}/valu
 yq -i e '.lanaBank.adminPanel.image.digest = strenv(admin_panel_image_digest)' ./charts/${CHARTS_SUBDIR}/values.yaml
 yq -i e '.lanaBank.customerPortal.image.digest = strenv(customer_portal_image_digest)' ./charts/${CHARTS_SUBDIR}/values.yaml
 yq -i e '.meltano.image.digest = strenv(meltano_image_digest)' ./charts/${CHARTS_SUBDIR}/values.yaml
+yq -i e '.dagster.image.digest = strenv(dagster_image_digest)' ./charts/${CHARTS_SUBDIR}/values.yaml
 sed -i "s|\(digest: \"${digest}\"\).*$|\1 # METADATA:: repository=https://github.com/GaloyMoney/${CHARTS_SUBDIR};commit_ref=${ref};app=${CHARTS_SUBDIR};|g" "./charts/${CHARTS_SUBDIR}/values.yaml"
 
 yq -i e '.lanaBank.appVersion = strenv(app_version)' ./charts/${CHARTS_SUBDIR}/values.yaml
