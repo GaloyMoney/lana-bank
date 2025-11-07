c6ca6b367e6de62fb8a92140f7be473e91d26cd9
Sebastien Verreault <sebver@pm.me>
2025-11-07 16:43:17 +0900

    fix: required core tables

--- dagster/src/dbt_lana_dw/models/staging/rollups/stg_core_committee_events_rollup.sql
+++
@@ -1,6 +0,0 @@
-with raw_stg_core_committee_events_rollup as (select * from {{ source("lana", "core_committee_events_rollup")}} )
-select
-    id as committee_id,
-    *,
-    TIMESTAMP_MICROS(CAST(CAST(_dlt_load_id AS DECIMAL) * 1e6 as INT64 )) as loaded_to_dw_at
-from raw_stg_core_committee_events_rollup
--- dagster/src/dbt_lana_dw/models/staging/rollups/stg_core_user_events_rollup.sql
+++
@@ -1,6 +0,0 @@
-with raw_stg_core_user_events_rollup as (select * from {{ source("lana", "core_user_events_rollup")}} )
-select
-    id as user_id,
-    *,
-    TIMESTAMP_MICROS(CAST(CAST(_dlt_load_id AS DECIMAL) * 1e6 as INT64 )) as loaded_to_dw_at
-from raw_stg_core_user_events_rollup
