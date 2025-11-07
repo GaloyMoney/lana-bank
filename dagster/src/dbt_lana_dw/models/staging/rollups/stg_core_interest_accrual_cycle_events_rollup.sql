3ab535c2e03c7738ed8bf6117cb8719318f0f99d
Sebastien Verreault <sebver@pm.me>
2025-11-07 11:25:52 +0900

    fix: staging rollups tables typo

--- dagster/src/dbt_lana_dw/models/staging/rollups/stg_core_interest_accrual_cycle_events_rollup.sql
+++ dagster/src/dbt_lana_dw/models/staging/rollups/stg_core_interest_accrual_cycle_events_rollup.sql
@@ -1,7 +1,7 @@
 with raw_stg_core_interest_accrual_cycle_events_rollup as (select * from {{ source("lana", "core_interest_accrual_cycle_events_rollup")}} )
 select
-    s.id as interest_accrual_cycle_id,
-    s.facility_id as credit_facility_id,
-    s.*,
+    id as interest_accrual_cycle_id,
+    facility_id as credit_facility_id,
+    *,
     TIMESTAMP_MICROS(CAST(CAST(_dlt_load_id AS DECIMAL) * 1e6 as INT64 )) as loaded_to_dw_at
 from raw_stg_core_interest_accrual_cycle_events_rollup
