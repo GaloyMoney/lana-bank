8c24a9c6b8272877e4b2d728d7970fa92538ab46
vindard <17693119+vindard@users.noreply.github.com>
2025-11-11 17:19:38 -0400

    refactor: renaming in 'AccountingClosingMetadata'

--- core/accounting/src/fiscal_year/ledger/closing.rs
+++ core/accounting/src/fiscal_year/ledger/closing.rs
@@ -25,7 +25,7 @@ impl AccountingClosingMetadata {
     pub(super) const METADATA_PATH: &'static str = "context.vars.account.metadata";
     pub(super) const METADATA_KEY: &'static str = "closing";
 
-    pub(super) fn update_metadata(
+    pub(super) fn update_with_monthly_closing(
         metadata: &mut serde_json::Value,
         closed_as_of: chrono::NaiveDate,
     ) {
@@ -195,18 +195,18 @@ mod tests {
         }
     }
 
-    mod update_metadata {
+    mod update_with_monthly_closing {
         use chrono::NaiveDate;
         use serde_json::json;
 
         use super::*;
 
         #[test]
-        fn can_update_metadata_with_empty_metadata() {
+        fn can_update_with_monthly_closing_with_empty_metadata() {
             let mut metadata = json!({});
             let closed_as_of = "2024-01-31".parse::<NaiveDate>().unwrap();
 
-            AccountingClosingMetadata::update_metadata(&mut metadata, closed_as_of);
+            AccountingClosingMetadata::update_with_monthly_closing(&mut metadata, closed_as_of);
 
             let closing_meta: AccountingClosingMetadata =
                 serde_json::from_value(metadata["closing"].clone()).unwrap();
@@ -214,7 +214,7 @@ mod tests {
         }
 
         #[test]
-        fn can_update_metadata_with_new_closing() {
+        fn can_update_with_monthly_closing_with_new_closing() {
             let existing_date = "2023-12-31";
             let existing_time = "2023-12-31T18:00:00Z".parse::<DateTime<Utc>>().unwrap();
             let mut metadata = json!({
@@ -227,7 +227,7 @@ mod tests {
             });
 
             let new_date = "2024-01-31".parse::<NaiveDate>().unwrap();
-            AccountingClosingMetadata::update_metadata(&mut metadata, new_date);
+            AccountingClosingMetadata::update_with_monthly_closing(&mut metadata, new_date);
 
             let closing_meta: AccountingClosingMetadata =
                 serde_json::from_value(metadata["closing"].clone()).unwrap();
@@ -236,14 +236,14 @@ mod tests {
         }
 
         #[test]
-        fn can_update_metadata_with_other_fields() {
+        fn can_update_with_monthly_closing_with_other_fields() {
             let mut metadata = json!({
                 "other_field": "value",
                 "another_field": 123
             });
             let closed_as_of = "2024-01-31".parse::<NaiveDate>().unwrap();
 
-            AccountingClosingMetadata::update_metadata(&mut metadata, closed_as_of);
+            AccountingClosingMetadata::update_with_monthly_closing(&mut metadata, closed_as_of);
 
             assert_eq!(metadata.get("other_field").unwrap(), "value");
             assert_eq!(metadata.get("another_field").unwrap(), 123);
