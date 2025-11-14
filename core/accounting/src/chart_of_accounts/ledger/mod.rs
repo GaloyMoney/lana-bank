0b557c22459699787670e14bedb20edadfc95951
nsandomeno <nsando61@gmail.com>
2025-11-14 16:02:19 -0500

    chore: undo coa fiscal_year dep graph

--- core/accounting/src/chart_of_accounts/ledger/mod.rs
+++ core/accounting/src/chart_of_accounts/ledger/mod.rs
@@ -1,11 +1,16 @@
+mod closing;
 pub mod error;
 
 use tracing::instrument;
 
 use cala_ledger::{
-    CalaLedger, DebitOrCredit, JournalId, LedgerOperation, account_set::NewAccountSet,
+    AccountSetId, CalaLedger, DebitOrCredit, JournalId, LedgerOperation, VelocityControlId,
+    VelocityLimitId,
+    account_set::NewAccountSet,
+    velocity::{NewBalanceLimit, NewLimit, NewVelocityControl, NewVelocityLimit, Params},
 };
 
+use closing::*;
 use error::*;
 
 use crate::Chart;
@@ -25,11 +30,11 @@ impl ChartLedger {
     }
 
     #[instrument(name = "chart_ledger.create_chart_root_account_set_in_op", skip(self, op, chart), fields(chart_id = %chart.id, chart_name = %chart.name), err)]
-    pub async fn create_chart_root_account_set_in_op<'a>(
+    pub async fn create_chart_root_account_set_in_op(
         &self,
-        op: es_entity::DbOp<'a>,
+        op: es_entity::DbOp<'_>,
         chart: &Chart,
-    ) -> Result<LedgerOperation<'a>, ChartLedgerError> {
+    ) -> Result<(), ChartLedgerError> {
         let mut ledger_op = self
             .cala
             .ledger_operation_from_db_op(op.with_db_time().await?);
@@ -49,6 +54,215 @@ impl ChartLedger {
             .create_in_op(&mut ledger_op, new_account_set)
             .await?;
 
-        Ok(ledger_op)
+        let control_id = self.create_close_control_in_op(&mut ledger_op).await?;
+
+        self.cala
+            .velocities()
+            .attach_control_to_account_set_in_op(
+                &mut ledger_op,
+                control_id,
+                chart.account_set_id,
+                Params::new(),
+            )
+            .await?;
+
+        ledger_op.commit().await?;
+        Ok(())
+    }
+
+    #[instrument(
+        name = "chart_ledger.attach_closing_controls_to_account_set_in_op",
+        skip(self, op),
+        err
+    )]
+    pub async fn attach_closing_controls_to_account_set_in_op(
+        &self,
+        mut op: LedgerOperation<'_>,
+        tracking_account_set_id: impl Into<AccountSetId> + std::fmt::Debug,
+    ) -> Result<VelocityControlId, ChartLedgerError> {
+        let tracking_account_set_id = tracking_account_set_id.into();
+        let control_id = self.create_close_control_in_op(&mut op).await?;
+
+        self.cala
+            .velocities()
+            .attach_control_to_account_set_in_op(
+                &mut op,
+                control_id,
+                tracking_account_set_id,
+                Params::new(),
+            )
+            .await?;
+
+        op.commit().await?;
+
+        Ok(control_id)
+    }
+
+    #[instrument(name = "chart_ledger.create_close_control_in_op", skip(self, op), err)]
+    async fn create_close_control_in_op(
+        &self,
+        op: &mut LedgerOperation<'_>,
+    ) -> Result<VelocityControlId, ChartLedgerError> {
+        let monthly_cel_conditions = AccountingClosingMetadata::monthly_cel_conditions();
+        let new_control = NewVelocityControl::builder()
+            .id(VelocityControlId::new())
+            .name("Account Closing")
+            .description("Control to restrict posting to closed accounts")
+            .condition(&monthly_cel_conditions)
+            .build()
+            .expect("build control");
+        let control = self
+            .cala
+            .velocities()
+            .create_control_in_op(op, new_control)
+            .await?;
+
+        // TODO: add_all to avoid n+1 ish issue
+        let AccountClosingLimits {
+            debit_settled: debit_settled_limit,
+            debit_pending: debit_pending_limit,
+            credit_settled: credit_settled_limit,
+            credit_pending: credit_pending_limit,
+        } = self.create_account_closing_limits_in_op(op).await?;
+
+        self.cala
+            .velocities()
+            .add_limit_to_control_in_op(op, control.id(), debit_settled_limit.id())
+            .await?;
+        self.cala
+            .velocities()
+            .add_limit_to_control_in_op(op, control.id(), debit_pending_limit.id())
+            .await?;
+        self.cala
+            .velocities()
+            .add_limit_to_control_in_op(op, control.id(), credit_settled_limit.id())
+            .await?;
+        self.cala
+            .velocities()
+            .add_limit_to_control_in_op(op, control.id(), credit_pending_limit.id())
+            .await?;
+
+        Ok(control.id())
+    }
+
+    #[instrument(
+        name = "chart_ledger.create_account_closing_limits_in_op",
+        skip(self, op),
+        err
+    )]
+    async fn create_account_closing_limits_in_op(
+        &self,
+        op: &mut LedgerOperation<'_>,
+    ) -> Result<AccountClosingLimits, ChartLedgerError> {
+        let velocity = self.cala.velocities();
+
+        let new_debit_settled_limit = NewVelocityLimit::builder()
+            .id(VelocityLimitId::new())
+            .name("Account Closed for debiting")
+            .description("Ensures no transactions allowed")
+            .window(vec![])
+            .limit(
+                NewLimit::builder()
+                    .balance(vec![
+                        NewBalanceLimit::builder()
+                            .layer("SETTLED")
+                            .amount("decimal('0')")
+                            .enforcement_direction("DEBIT")
+                            .build()
+                            .expect("limit"),
+                    ])
+                    .build()
+                    .expect("limit"),
+            )
+            .params(vec![])
+            .build()
+            .expect("build limit");
+
+        let new_debit_pending_limit = NewVelocityLimit::builder()
+            .id(VelocityLimitId::new())
+            .name("Account Closed for debiting")
+            .description("Ensures no transactions allowed")
+            .window(vec![])
+            .limit(
+                NewLimit::builder()
+                    .balance(vec![
+                        NewBalanceLimit::builder()
+                            .layer("PENDING")
+                            .amount("decimal('0')")
+                            .enforcement_direction("DEBIT")
+                            .build()
+                            .expect("limit"),
+                    ])
+                    .build()
+                    .expect("limit"),
+            )
+            .params(vec![])
+            .build()
+            .expect("build limit");
+
+        let new_credit_settled_limit = NewVelocityLimit::builder()
+            .id(VelocityLimitId::new())
+            .name("Account Closed for crediting")
+            .description("Ensures no transactions allowed")
+            .window(vec![])
+            .limit(
+                NewLimit::builder()
+                    .balance(vec![
+                        NewBalanceLimit::builder()
+                            .layer("SETTLED")
+                            .amount("decimal('0')")
+                            .enforcement_direction("CREDIT")
+                            .build()
+                            .expect("limit"),
+                    ])
+                    .build()
+                    .expect("limit"),
+            )
+            .params(vec![])
+            .build()
+            .expect("build limit");
+
+        let new_credit_pending_limit = NewVelocityLimit::builder()
+            .id(VelocityLimitId::new())
+            .name("Account Closed for crediting")
+            .description("Ensures no transactions allowed")
+            .window(vec![])
+            .limit(
+                NewLimit::builder()
+                    .balance(vec![
+                        NewBalanceLimit::builder()
+                            .layer("PENDING")
+                            .amount("decimal('0')")
+                            .enforcement_direction("CREDIT")
+                            .build()
+                            .expect("limit"),
+                    ])
+                    .build()
+                    .expect("limit"),
+            )
+            .params(vec![])
+            .build()
+            .expect("build limit");
+
+        // TODO: create_all to avoid n+1-ish issue
+        let debit_settled_limit = velocity
+            .create_limit_in_op(op, new_debit_settled_limit)
+            .await?;
+        let debit_pending_limit = velocity
+            .create_limit_in_op(op, new_debit_pending_limit)
+            .await?;
+        let credit_settled_limit = velocity
+            .create_limit_in_op(op, new_credit_settled_limit)
+            .await?;
+        let credit_pending_limit = velocity
+            .create_limit_in_op(op, new_credit_pending_limit)
+            .await?;
+
+        Ok(AccountClosingLimits {
+            debit_settled: debit_settled_limit,
+            debit_pending: debit_pending_limit,
+            credit_settled: credit_settled_limit,
+            credit_pending: credit_pending_limit,
+        })
     }
 }
