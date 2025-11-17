3618f7bc7e53c731e179edd7edb6c8fa17b0cb7a
Nicolas Burtey <nb@galoy.io>
2025-11-16 19:36:50 -0600

    chore: ensure a single job is running for publishing daily event

---
+++ core/time-events/src/job.rs
@@ -0,0 +1,122 @@
+use async_trait::async_trait;
+use serde::{Deserialize, Serialize};
+use tracing::instrument;
+
+use job::{CurrentJob, Job, JobCompletion, JobConfig, JobInitializer, JobRunner, JobType};
+use outbox::{Outbox, OutboxEventMarker};
+
+use crate::{TimeEvent, broadcaster::DailyClosingBroadcaster, config::TimeEventsConfig};
+
+#[derive(Serialize, Deserialize)]
+pub struct DailyClosingBroadcasterJobConfig<E>
+where
+    E: OutboxEventMarker<TimeEvent>,
+{
+    _phantom: std::marker::PhantomData<E>,
+}
+
+impl<E> Default for DailyClosingBroadcasterJobConfig<E>
+where
+    E: OutboxEventMarker<TimeEvent>,
+{
+    fn default() -> Self {
+        Self {
+            _phantom: std::marker::PhantomData,
+        }
+    }
+}
+
+impl<E> DailyClosingBroadcasterJobConfig<E>
+where
+    E: OutboxEventMarker<TimeEvent>,
+{
+    pub fn new() -> Self {
+        Self::default()
+    }
+}
+
+impl<E> JobConfig for DailyClosingBroadcasterJobConfig<E>
+where
+    E: OutboxEventMarker<TimeEvent> + Send + Sync + 'static,
+{
+    type Initializer = DailyClosingBroadcasterInit<E>;
+}
+
+pub struct DailyClosingBroadcasterInit<E>
+where
+    E: OutboxEventMarker<TimeEvent>,
+{
+    outbox: Outbox<E>,
+    config: TimeEventsConfig,
+}
+
+impl<E> DailyClosingBroadcasterInit<E>
+where
+    E: OutboxEventMarker<TimeEvent>,
+{
+    pub fn new(outbox: &Outbox<E>, config: TimeEventsConfig) -> Self {
+        Self {
+            outbox: outbox.clone(),
+            config,
+        }
+    }
+}
+
+const DAILY_CLOSING_BROADCASTER: JobType = JobType::new("time-events.daily-closing-broadcaster");
+
+impl<E> JobInitializer for DailyClosingBroadcasterInit<E>
+where
+    E: OutboxEventMarker<TimeEvent> + Send + Sync + 'static,
+{
+    fn job_type() -> JobType
+    where
+        Self: Sized,
+    {
+        DAILY_CLOSING_BROADCASTER
+    }
+
+    #[instrument(name = "time_events.daily_closing_broadcaster_job.init", skip(self))]
+    fn init(&self, _: &Job) -> Result<Box<dyn JobRunner>, Box<dyn std::error::Error>> {
+        Ok(Box::new(DailyClosingBroadcasterJobRunner {
+            broadcaster: Some(DailyClosingBroadcaster::new(
+                &self.outbox,
+                self.config.clone(),
+            )),
+        }))
+    }
+}
+
+pub struct DailyClosingBroadcasterJobRunner<E>
+where
+    E: OutboxEventMarker<TimeEvent>,
+{
+    broadcaster: Option<DailyClosingBroadcaster<E>>,
+}
+
+#[async_trait]
+impl<E> JobRunner for DailyClosingBroadcasterJobRunner<E>
+where
+    E: OutboxEventMarker<TimeEvent> + Send + Sync + 'static,
+{
+    #[instrument(name = "time_events.daily_closing_broadcaster_job.run", skip_all, err)]
+    async fn run(
+        &self,
+        _current_job: CurrentJob,
+    ) -> Result<JobCompletion, Box<dyn std::error::Error>> {
+        // SAFETY: broadcaster is only None after we take it, and run() is only called once
+        let broadcaster = self
+            .broadcaster
+            .as_ref()
+            .expect("Broadcaster already consumed");
+
+        // Clone the config and outbox to create a new broadcaster
+        let broadcaster = DailyClosingBroadcaster::new(
+            &broadcaster.outbox().clone(),
+            broadcaster.config().clone(),
+        );
+
+        broadcaster.run().await;
+        // This should never return, but if it does, reschedule immediately
+        Ok(JobCompletion::RescheduleNow)
+    }
+}
