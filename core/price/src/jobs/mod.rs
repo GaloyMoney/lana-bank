94eb8af0a66d65d50f109ebec591b443d3a12317
Vaibhav <thevaibhavdixit@gmail.com>
2025-11-18 14:35:45 +0530

    refactor: spawn thread to listen to price updates via event

--- core/price/src/jobs/mod.rs
+++ core/price/src/jobs/mod.rs
@@ -1,2 +1,1 @@
 pub mod get_price_from_bfx;
-pub mod update_price_in_cache;
--- core/price/src/lib.rs
+++ core/price/src/lib.rs
@@ -1,40 +1,37 @@
 #![cfg_attr(feature = "fail-on-warnings", deny(warnings))]
 #![cfg_attr(feature = "fail-on-warnings", deny(clippy::all))]
-
 pub mod bfx_client;
-mod cache;
 pub mod error;
 mod event;
 pub mod jobs;
 mod primitives;
 
 use error::PriceError;
+use futures::StreamExt;
 use job::Jobs;
 use outbox::{Outbox, OutboxEventMarker};
+use std::sync::Arc;
+use tokio::sync::RwLock;
+use tokio::task::JoinHandle;
 
-pub use cache::PriceCache;
 pub use event::*;
 pub use primitives::*;
 
 #[derive(Clone)]
 pub struct Price {
-    cache: PriceCache,
+    inner: Arc<RwLock<PriceOfOneBTC>>,
+    _handle: Arc<JoinHandle<()>>,
 }
 
 impl Price {
-    pub fn usd_cents_per_btc(&self) -> Result<PriceOfOneBTC, PriceError> {
-        Ok(self.cache.get_price())
-    }
-
-    pub async fn init<E>(jobs: &Jobs, outbox: &Outbox<E>) -> Result<Self, PriceError>
+    pub async fn init<E>(jobs: &Jobs, outbox: Outbox<E>) -> Result<Self, PriceError>
     where
-        E: OutboxEventMarker<CorePriceEvent>,
+        E: OutboxEventMarker<CorePriceEvent> + Send + Sync + 'static,
     {
-        // this has to change , either give no price available error or source it from outbox on startup
-        let cache = PriceCache::new(PriceOfOneBTC::ZERO);
+        let price = Arc::new(RwLock::new(PriceOfOneBTC::ZERO));
 
         jobs.add_initializer_and_spawn_unique(
-            jobs::get_price_from_bfx::GetPriceFromClientJobInit::<E>::new(outbox),
+            jobs::get_price_from_bfx::GetPriceFromClientJobInit::<E>::new(&outbox),
             jobs::get_price_from_bfx::GetPriceFromClientJobConfig::<E> {
                 _phantom: std::marker::PhantomData,
             },
@@ -42,15 +39,32 @@ impl Price {
         .await
         .map_err(PriceError::JobError)?;
 
-        jobs.add_initializer_and_spawn_unique(
-            jobs::update_price_in_cache::UpdatePriceInCacheInit::<E>::new(outbox, &cache),
-            jobs::update_price_in_cache::UpdatePriceInCacheJobConfig::<E> {
-                _phantom: std::marker::PhantomData,
-            },
-        )
-        .await
-        .map_err(PriceError::JobError)?;
+        let price_clone = Arc::clone(&price);
 
-        Ok(Self { cache })
+        let handle = tokio::spawn(async move {
+            let mut stream = match outbox.listen_ephemeral().await {
+                Ok(s) => s,
+                Err(_) => return,
+            };
+
+            while let Some(message) = stream.next().await {
+                if message.event_type.as_str() == PRICE_UPDATED_EVENT_TYPE {
+                    if let Some(CorePriceEvent::PriceUpdated { price: new_price }) =
+                        message.payload.as_event()
+                    {
+                        *price_clone.write().await = *new_price;
+                    }
+                }
+            }
+        });
+
+        Ok(Self {
+            inner: price,
+            _handle: Arc::new(handle),
+        })
+    }
+
+    pub async fn usd_cents_per_btc(&self) -> PriceOfOneBTC {
+        *self.inner.read().await
     }
 }
