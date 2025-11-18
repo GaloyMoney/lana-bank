0fccbd0171cb069c72d4988a7090abcf875f4ea5
nicolasburtey <nicolasburtey@users.noreply.github.com>
2025-11-18 13:19:39 -0600

    chore: using display directly in macro (#3056)
    * chore: using display directly in macro
    
    * chore: removing customer_id

--- core/customer/src/customer_activity_repo.rs
+++ core/customer/src/customer_activity_repo.rs
@@ -16,8 +16,8 @@ impl CustomerActivityRepo {
 
     #[tracing::instrument(
         name = "customer_activity.upsert_activity",
-        skip(self, customer_id),
-        fields(customer_id = field::Empty),
+        skip(self),
+        fields(customer_id = %customer_id),
         err(level = "warn")
     )]
     pub async fn upsert_activity(
@@ -26,7 +26,6 @@ impl CustomerActivityRepo {
         activity_date: chrono::DateTime<chrono::Utc>,
     ) -> Result<(), CustomerError> {
         let customer_uuid: uuid::Uuid = customer_id.into();
-        Span::current().record("customer_id", customer_uuid.to_string());
         sqlx::query!(
             r#"
             INSERT INTO customer_activity (customer_id, last_activity_date)
