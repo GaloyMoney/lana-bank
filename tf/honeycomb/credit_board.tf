54d9757aa3ddeafc39708a6fed976e13389f8dfd
Vaibhav <thevaibhavdixit@gmail.com>
2025-11-25 17:01:41 +0530

    feat: credit facility query for board

--- tf/honeycomb/credit_board.tf
+++ tf/honeycomb/credit_board.tf
@@ -56,6 +56,35 @@ resource "honeycombio_query_annotation" "pending_credit_facilities" {
   name     = "Pending Credit Facility queries"
 }
 
+data "honeycombio_query_specification" "credit_facilities" {
+  calculation {
+    op = "COUNT"
+  }
+
+  filter {
+    column = "credit_facility_id"
+    op     = "exists"
+  }
+
+  breakdowns = ["trace.trace_id", "credit_facility_id", "root.name"]
+
+  order {
+    op    = "COUNT"
+    order = "descending"
+  }
+
+  time_range = 604800
+}
+
+resource "honeycombio_query" "credit_facilities" {
+  query_json = data.honeycombio_query_specification.credit_facilities.json
+}
+
+resource "honeycombio_query_annotation" "credit_facilities" {
+  query_id = honeycombio_query.credit_facilities.id
+  name     = "Credit Facility queries"
+}
+
 # Credit Board
 resource "honeycombio_flexible_board" "credit_board" {
   name        = "${local.name_prefix}-credit"
@@ -75,6 +104,15 @@ resource "honeycombio_flexible_board" "credit_board" {
     query_panel {
       query_id            = honeycombio_query.pending_credit_facilities.id
       query_annotation_id = honeycombio_query_annotation.pending_credit_facilities.id
+      query_style         = "graph"
+    }
+  }
+
+  panel {
+    type = "query"
+    query_panel {
+      query_id            = honeycombio_query.credit_facilities.id
+      query_annotation_id = honeycombio_query_annotation.credit_facilities.id
       query_style         = "graph"
     }
   }
