d819f11e87fcfda48490f1c10b4a2e7047567d29
Vaibhav <thevaibhavdixit@gmail.com>
2025-12-01 21:03:37 +0530

    refactor: collateral under facility module

--- core/fcredit/facility/src/publisher.rs
+++ core/fcredit/facility/src/publisher.rs
@@ -2,6 +2,7 @@ use outbox::{Outbox, OutboxEventMarker};
 use tracing::instrument;
 
 use crate::{
+    collateral::{Collateral, CollateralEvent},
     error::CoreCreditFacilityError,
     event::CoreCreditFacilityEvent,
     primitives::CreditFacilityProposalStatus,
@@ -65,6 +66,50 @@ where
         self.outbox
             .publish_all_persisted(op, publish_events)
             .await?;
+        Ok(())
+    }
+
+    #[instrument(
+        name = "credit.publisher.publish_collateral",
+        skip_all,
+        err(level = "warn")
+    )]
+    pub async fn publish_collateral(
+        &self,
+        op: &mut impl es_entity::AtomicOperation,
+        entity: &Collateral,
+        new_events: es_entity::LastPersisted<'_, CollateralEvent>,
+    ) -> Result<(), CoreCreditFacilityError> {
+        use CollateralEvent::*;
+        let events = new_events
+            .filter_map(|event| match &event.event {
+                UpdatedViaManualInput {
+                    abs_diff,
+                    action,
+                    ledger_tx_id,
+                    ..
+                }
+                | UpdatedViaCustodianSync {
+                    abs_diff,
+                    action,
+                    ledger_tx_id,
+                    ..
+                } => Some(CoreCreditFacilityEvent::FacilityCollateralUpdated {
+                    ledger_tx_id: *ledger_tx_id,
+                    abs_diff: *abs_diff,
+                    action: *action,
+                    recorded_at: event.recorded_at,
+                    effective: event.recorded_at.date_naive(),
+                    new_amount: entity.amount,
+                    credit_facility_id: entity.credit_facility_id,
+                    pending_credit_facility_id: entity.pending_credit_facility_id,
+                }),
+                _ => None,
+            })
+            .collect::<Vec<_>>();
+
+        self.outbox.publish_all_persisted(op, events).await?;
+
         Ok(())
     }
 }
