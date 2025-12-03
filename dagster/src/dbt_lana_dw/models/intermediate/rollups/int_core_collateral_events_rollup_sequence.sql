1cb2aa64d2258433b1069f9ae94ae7023a67edf5
sebastienverreault <sv@galoy.io>
2025-12-03 15:34:08 +0900

    feat: add int event rollup tables

---
+++ dagster/src/dbt_lana_dw/models/intermediate/rollups/int_core_collateral_events_rollup_sequence.sql
@@ -0,0 +1,38 @@
+{{ config(
+    unique_key = ['collateral_id', 'version'],
+) }}
+
+
+with source as (
+    select s.*
+    from {{ ref('stg_core_collateral_events_rollup') }} as s
+),
+
+transformed as (
+    select
+        collateral_id,
+        credit_facility_id,
+
+        action,
+        cast(abs_diff as numeric) as abs_diff_sats,
+        cast(abs_diff as numeric) / {{ var('sats_per_bitcoin') }} as abs_diff_btc,
+        cast(collateral_amount as numeric) as collateral_amount_sats,
+        cast(collateral_amount as numeric) / {{ var('sats_per_bitcoin') }} as collateral_amount_btc,
+        account_id,
+        created_at as collateral_created_at,
+        modified_at as collateral_modified_at,
+
+        * except (
+            collateral_id,
+            credit_facility_id,
+            action,
+            abs_diff,
+            collateral_amount,
+            account_id,
+            created_at,
+            modified_at
+        )
+    from source
+)
+
+select * from transformed
---
+++ dagster/src/dbt_lana_dw/models/intermediate/rollups/int_core_liquidation_process_events_rollup_sequence.sql
@@ -0,0 +1,35 @@
+{{ config(
+    unique_key = ['liquidation_process_id', 'version'],
+) }}
+
+
+with source as (
+    select s.*
+    from {{ ref('stg_core_liquidation_process_events_rollup') }} as s
+),
+
+transformed as (
+    select
+        liquidation_process_id,
+        credit_facility_id,
+
+        cast(effective as timestamp) as effective,
+        is_completed,
+        cast(initial_amount as numeric) / {{ var('cents_per_usd') }} as initial_amount_usd,
+        created_at as liquidation_process_created_at,
+        modified_at as liquidation_process_modified_at,
+
+        * except (
+            liquidation_process_id,
+            credit_facility_id,
+
+            effective,
+            is_completed,
+            initial_amount,
+            created_at,
+            modified_at
+        )
+    from source
+)
+
+select * from transformed
