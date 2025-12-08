1dcbaadb8ec3de1761b1817a7eb644fb090950c2
pmartincalvo <pablomartincalvo@gmail.com>
2025-12-08 09:27:26 +0100

    chore: explicit column selection in dbt models (#3165)
    * chore: dbt automation and test, small refactors
    
    * chore: fmt
    
    * chore: remove unneeded type guard rail
    
    * chore: remove bloaty comment
    
    * chore: remove over-commenting from test
    
    * chore: remove unused freshness checks
    
    * chore: avoid select *, use explict column listing
    
    * chore: avoid select *, use explict column listing

--- dagster/src/dbt_lana_dw/models/intermediate/rollups/int_core_deposit_account_events_rollup_sequence.sql
+++ dagster/src/dbt_lana_dw/models/intermediate/rollups/int_core_deposit_account_events_rollup_sequence.sql
@@ -1,6 +1,14 @@
 with source as (
     select
-        s.*
+        deposit_account_id,
+        version,
+        created_at,
+        modified_at,
+        account_holder_id,
+        account_ids,
+        public_id,
+        status,
+        loaded_to_dw_at
     from {{ ref('stg_core_deposit_account_events_rollup') }} as s
 )
 
@@ -12,14 +20,23 @@ with source as (
         created_at as deposit_account_created_at,
         modified_at as deposit_account_modified_at,
 
-        * except(
-            deposit_account_id,
-            account_holder_id,
-            created_at,
-            modified_at
-        )
+        version,
+        account_ids,
+        public_id,
+        status,
+        loaded_to_dw_at
     from source
 )
 
 
-select * from transformed
+select
+    deposit_account_id,
+    customer_id,
+    deposit_account_created_at,
+    deposit_account_modified_at,
+    version,
+    account_ids,
+    public_id,
+    status,
+    loaded_to_dw_at
+from transformed
