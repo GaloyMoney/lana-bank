628ff9313ce4b7c74970771430a39fef85ddde66
nsandomeno <nsando61@gmail.com>
2025-12-08 13:37:58 -0500

    chore: call coa post closing tx use-case from fiscal year domain service

--- core/accounting/src/fiscal_year/mod.rs
+++ core/accounting/src/fiscal_year/mod.rs
@@ -2,6 +2,8 @@ mod entity;
 pub mod error;
 mod repo;
 
+use std::str::FromStr;
+
 use chrono::NaiveDate;
 use tracing::instrument;
 
@@ -11,9 +13,9 @@ use es_entity::{Idempotent, PaginatedQueryArgs};
 use tracing_macros::record_error_severity;
 
 use crate::{
-    FiscalYearId,
+    AccountCode, FiscalYearId,
     chart_of_accounts::ChartOfAccounts,
-    primitives::{ChartId, CoreAccountingAction, CoreAccountingObject},
+    primitives::{CalaTxId, ChartId, ClosingTxSpec, CoreAccountingAction, CoreAccountingObject},
 };
 
 #[cfg(feature = "json-schema")]
@@ -152,12 +154,27 @@ where
             .await?;
         let mut fiscal_year = self.repo.find_by_id(id).await?;
         let now = crate::time::now();
-        match fiscal_year.close(now)? {
-            Idempotent::Executed(_) => {
+        let tx_id = CalaTxId::new();
+        match fiscal_year.close(now, tx_id)? {
+            Idempotent::Executed(closed_as_of) => {
                 let mut op = self.repo.begin_op().await?;
                 self.repo.update_in_op(&mut op, &mut fiscal_year).await?;
-                // TODO: Operate on ledger via `chart_of_accounts`.
-                op.commit().await?;
+                // TODO: read configuration that depends on and extends existing config
+                // related to the `ProfitAndLossStatement` and `BalanceSheet`.
+                let closing_tx_spec = ClosingTxSpec::new(
+                    tx_id,
+                    AccountCode::from_str("4").unwrap(),
+                    AccountCode::from_str("5").unwrap(),
+                    AccountCode::from_str("6").unwrap(),
+                    AccountCode::from_str("32.01").unwrap(),
+                    AccountCode::from_str("32.02").unwrap(),
+                    fiscal_year.reference.clone(),
+                    fiscal_year.opened_as_of,
+                    closed_as_of,
+                );
+                self.chart_of_accounts
+                    .post_closing_transaction(op, sub, fiscal_year.chart_id, closing_tx_spec)
+                    .await?;
 
                 Ok(fiscal_year)
             }
