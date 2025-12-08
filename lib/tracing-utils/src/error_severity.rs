740cb44095c4edc99493d7ffe133243f6ec131b2
vindard <17693119+vindard@users.noreply.github.com>
2025-12-08 17:16:57 -0400

    chore: remove sqlx ErrorSeverity implementation

--- lib/tracing-utils/src/error_severity.rs
+++ lib/tracing-utils/src/error_severity.rs
@@ -14,26 +14,3 @@ impl ErrorSeverity for es_entity::EsEntityError {
         }
     }
 }
-
-/// Implementation for sqlx::Error - provides appropriate severity levels for database errors
-impl ErrorSeverity for sqlx::Error {
-    fn severity(&self) -> Level {
-        match self {
-            // Database errors are generally warnings (could be transient)
-            sqlx::Error::Database(_) => Level::WARN,
-            // Connection errors could be temporary
-            sqlx::Error::PoolTimedOut => Level::WARN,
-            sqlx::Error::PoolClosed => Level::WARN,
-            sqlx::Error::Io(_) => Level::WARN,
-            // Configuration/usage errors are more serious
-            sqlx::Error::Configuration(_) => Level::ERROR,
-            sqlx::Error::TypeNotFound { .. } => Level::ERROR,
-            sqlx::Error::ColumnNotFound(_) => Level::ERROR,
-            sqlx::Error::ColumnDecode { .. } => Level::ERROR,
-            sqlx::Error::Decode(_) => Level::ERROR,
-            sqlx::Error::Migrate(_) => Level::ERROR,
-            // Other errors default to ERROR
-            _ => Level::ERROR,
-        }
-    }
-}
