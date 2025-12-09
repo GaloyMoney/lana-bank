ad2a2bbfa9989edc04c54d4e9293ffdd0726455d
vindard <17693119+vindard@users.noreply.github.com>
2025-12-09 10:46:48 -0400

    chore: implement ErrorSeverity for errors in Arc

--- lib/tracing-utils/src/error_severity.rs
+++ lib/tracing-utils/src/error_severity.rs
@@ -1,3 +1,5 @@
+use std::sync::Arc;
+
 use tracing::Level;
 
 /// Trait for errors that can provide their own severity level for tracing
@@ -18,5 +20,15 @@ impl ErrorSeverity for es_entity::EsEntityError {
 impl ErrorSeverity for std::io::Error {
     fn severity(&self) -> Level {
         Level::ERROR
+    }
+}
+
+impl<E> ErrorSeverity for Arc<E>
+where
+    E: ErrorSeverity + ?Sized,
+{
+    fn severity(&self) -> tracing::Level {
+        // delegate to the inner error
+        (**self).severity()
     }
 }
