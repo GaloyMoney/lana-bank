73423bc2a476e4b5356a19f07e5d18925048b255
sebastienverreault <sv@galoy.io>
2025-12-10 13:21:48 +0900

    fix: rebase fockup

--- dagster/src/dbt_lana_dw/models/staging/rollups/stg_core_chart_events_rollup.sql
+++ dagster/src/dbt_lana_dw/models/staging/rollups/stg_core_chart_events_rollup.sql
@@ -1,6 +1,29 @@
-with raw_stg_core_chart_events_rollup as (select * from {{ source("lana", "core_chart_events_rollup")}} )
+with raw_stg_core_chart_events_rollup as (
+    select
+        id,
+        version,
+        created_at,
+        modified_at,
+        account_set_id,
+        closed_as_of,
+        first_period_opened_as_of,
+        first_period_opened_at,
+        name,
+        reference,
+        _dlt_load_id,
+        _dlt_id
+    from {{ source("lana", "core_chart_events_rollup")}}
+)
 select
     id as chart_id,
-    *,
+    version,
+    created_at,
+    modified_at,
+    account_set_id,
+    closed_as_of,
+    first_period_opened_as_of,
+    first_period_opened_at,
+    name,
+    reference,
     TIMESTAMP_MICROS(CAST(CAST(_dlt_load_id AS DECIMAL) * 1e6 as INT64 )) as loaded_to_dw_at
 from raw_stg_core_chart_events_rollup
