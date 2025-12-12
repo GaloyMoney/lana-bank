484bdc1bb51b97d859df64a7b07da991c83946ac
nsandomeno <nsando61@gmail.com>
2025-12-12 16:59:19 -0500

    chore: update schema

--- lana/app/migrations/20251212215830_create_core_fiscal_year_events_rollup.sql
+++ lana/app/migrations/20251212215830_create_core_fiscal_year_events_rollup.sql
@@ -11,8 +11,7 @@ CREATE TABLE core_fiscal_year_events_rollup (
   month_closed_as_of VARCHAR,
   month_closed_at TIMESTAMPTZ,
   opened_as_of VARCHAR,
-  reference VARCHAR,
-  tx_id UUID
+  reference VARCHAR
 ,
   PRIMARY KEY (id, version)
 );
@@ -54,7 +53,6 @@ BEGIN
     new_row.month_closed_at := (NEW.event ->> 'month_closed_at')::TIMESTAMPTZ;
     new_row.opened_as_of := (NEW.event ->> 'opened_as_of');
     new_row.reference := (NEW.event ->> 'reference');
-    new_row.tx_id := (NEW.event ->> 'tx_id')::UUID;
   ELSE
     -- Default all fields to current values
     new_row.chart_id := current_row.chart_id;
@@ -64,7 +62,6 @@ BEGIN
     new_row.month_closed_at := current_row.month_closed_at;
     new_row.opened_as_of := current_row.opened_as_of;
     new_row.reference := current_row.reference;
-    new_row.tx_id := current_row.tx_id;
   END IF;
 
   -- Update only the fields that are modified by the specific event
@@ -79,7 +76,6 @@ BEGIN
     WHEN 'year_closed' THEN
       new_row.closed_as_of := (NEW.event ->> 'closed_as_of');
       new_row.closed_at := (NEW.event ->> 'closed_at')::TIMESTAMPTZ;
-      new_row.tx_id := (NEW.event ->> 'tx_id')::UUID;
   END CASE;
 
   INSERT INTO core_fiscal_year_events_rollup (
@@ -93,8 +89,7 @@ BEGIN
     month_closed_as_of,
     month_closed_at,
     opened_as_of,
-    reference,
-    tx_id
+    reference
   )
   VALUES (
     new_row.id,
@@ -107,8 +102,7 @@ BEGIN
     new_row.month_closed_as_of,
     new_row.month_closed_at,
     new_row.opened_as_of,
-    new_row.reference,
-    new_row.tx_id
+    new_row.reference
   );
 
   RETURN NEW;
