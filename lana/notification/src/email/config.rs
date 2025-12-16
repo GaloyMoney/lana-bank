9716eed42f562451970ef9ef2e2fbc847f513903
Nicolas Burtey <nb@galoy.io>
2025-12-15 19:32:53 -0600

    chore: some feedback on the API

--- lana/notification/src/email/config.rs
+++ lana/notification/src/email/config.rs
@@ -53,13 +53,23 @@ impl ConfigSpec for NotificationEmailConfigSpec {
     }
 
     fn validate(value: &NotificationEmailConfig) -> Result<(), DomainConfigError> {
+        // DomainConfigError is smelly here
+        // we're looking at if an email exist and is well formatted
+        // this has nothing to do with DomainConfig really
+        // it's proper business logic to the notification email
         if value.from_email.trim().is_empty() {
+
             return Err(DomainConfigError::InvalidState(
                 "from_email is required".to_string(),
             ));
         }
 
         if value.from_name.trim().is_empty() {
+            // from name can be as long as one want and create
+            // some burden to postgres
+            //
+            // the default implementation is overriden here
+            // those function are not additional, it's this one OR the default. 
             return Err(DomainConfigError::InvalidState(
                 "from_name is required".to_string(),
             ));
