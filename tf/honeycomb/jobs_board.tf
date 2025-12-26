325bae0dc9daee74fa7966b2c8783c28c7505977
nicolasburtey <nicolasburtey@users.noreply.github.com>
2025-12-26 14:01:02 -0600

    chore: merging jobs boards (#3273)

--- tf/honeycomb/jobs_board.tf
+++ tf/honeycomb/jobs_board.tf
@@ -106,10 +106,108 @@ resource "honeycombio_query_annotation" "concurrent_modification_errors" {
   name     = "Concurrent modification errors in jobs"
 }
 
+data "honeycombio_query_specification" "process_type" {
+  calculation {
+    op = "COUNT"
+  }
+
+  filter {
+    column = "event_type"
+    op     = "exists"
+  }
+
+  filter {
+    column = "process_type"
+    op     = "exists"
+  }
+
+  breakdowns = ["seq", "event_type", "name", "process_type"]
+
+  order {
+    column = "seq"
+    order  = "descending"
+  }
+
+  time_range = 604800
+}
+
+resource "honeycombio_query" "process_type" {
+  dataset    = var.honeycomb_dataset
+  query_json = data.honeycombio_query_specification.process_type.json
+}
+
+resource "honeycombio_query_annotation" "process_type" {
+  dataset  = var.honeycomb_dataset
+  query_id = honeycombio_query.process_type.id
+  name     = "Governance approval queries"
+}
+
+data "honeycombio_query_specification" "events" {
+  calculation {
+    op = "COUNT"
+  }
+
+  filter {
+    column = "event_type"
+    op     = "exists"
+  }
+
+  breakdowns = ["seq", "event_type", "name"]
+
+  order {
+    column = "seq"
+    order  = "descending"
+  }
+
+  time_range = 604800
+}
+
+resource "honeycombio_query" "events" {
+  dataset    = var.honeycomb_dataset
+  query_json = data.honeycombio_query_specification.events.json
+}
+
+resource "honeycombio_query_annotation" "events" {
+  dataset  = var.honeycomb_dataset
+  query_id = honeycombio_query.events.id
+  name     = "Events job queries"
+}
+
+data "honeycombio_query_specification" "handled" {
+  calculation {
+    op = "COUNT"
+  }
+
+  filter {
+    column = "handled"
+    op     = "exists"
+  }
+
+  breakdowns = ["seq", "handled"]
+
+  order {
+    column = "seq"
+    order  = "descending"
+  }
+
+  time_range = 604800
+}
+
+resource "honeycombio_query" "handled" {
+  dataset    = var.honeycomb_dataset
+  query_json = data.honeycombio_query_specification.handled.json
+}
+
+resource "honeycombio_query_annotation" "handled" {
+  dataset  = var.honeycomb_dataset
+  query_id = honeycombio_query.handled.id
+  name     = "handled job queries"
+}
+
 # Jobs dashboard
 resource "honeycombio_flexible_board" "jobs" {
   name        = "${local.name_prefix}-jobs"
-  description = "Job execution metrics for ${local.name_prefix}"
+  description = "Job execution and event processing metrics for ${local.name_prefix}"
 
   panel {
     type = "query"
@@ -140,5 +238,34 @@ resource "honeycombio_flexible_board" "jobs" {
       query_style         = "graph"
     }
   }
-}
 
+  panel {
+    type = "query"
+
+    query_panel {
+      query_id            = honeycombio_query.process_type.id
+      query_annotation_id = honeycombio_query_annotation.process_type.id
+      query_style         = "graph"
+    }
+  }
+
+  panel {
+    type = "query"
+
+    query_panel {
+      query_id            = honeycombio_query.events.id
+      query_annotation_id = honeycombio_query_annotation.events.id
+      query_style         = "graph"
+    }
+  }
+
+  panel {
+    type = "query"
+
+    query_panel {
+      query_id            = honeycombio_query.handled.id
+      query_annotation_id = honeycombio_query_annotation.handled.id
+      query_style         = "graph"
+    }
+  }
+}
