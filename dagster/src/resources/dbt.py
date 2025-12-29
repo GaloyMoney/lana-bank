58b345d5ca81e10317ee5ed9c5846a111667a7b6
pablo <pablomartincalvo@gmail.com>
2025-12-29 12:49:25 +0100

    chore: udf functions created once at start

--- dagster/src/resources/dbt.py
+++ dagster/src/resources/dbt.py
@@ -7,3 +7,6 @@ RESOURCE_KEY_LANA_DBT = "dbt"
 dbt_resource = DbtCliResource(project_dir=Path("/lana-dw/src/dbt_lana_dw/"))
 dbt_parse_invocation = dbt_resource.cli(["parse"], manifest={}).wait()
 DBT_MANIFEST_PATH = dbt_parse_invocation.target_path.joinpath("manifest.json")
+
+# Run UDF creation once at startup (instead of on-run-start hook on every model run)
+dbt_resource.cli(["run-operation", "create_udfs"]).wait()
