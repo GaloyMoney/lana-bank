8be6d63604de15a3e2fd0549b3e56caa449e643a
Vaibhav <thevaibhavdixit@gmail.com>
2026-01-09 12:27:30 +0530

    refactor: notifications to use new job

--- lana/app/src/app/mod.rs
+++ lana/app/src/app/mod.rs
@@ -204,7 +204,7 @@ impl LanaApp {
 
         Notification::init(
             config.notification,
-            &jobs,
+            &mut job_new,
             &outbox,
             access.users(),
             &credit,
