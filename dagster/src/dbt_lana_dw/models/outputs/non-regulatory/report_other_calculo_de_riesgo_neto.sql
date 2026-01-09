970cce678d3758f54eae0ea1923ee8b6e74fc93d
Satoshi <sv@galoy.io>
2026-01-09 13:55:43 +0000

    fix: fmt

--- dagster/src/dbt_lana_dw/models/outputs/non-regulatory/report_other_calculo_de_riesgo_neto.sql
+++ dagster/src/dbt_lana_dw/models/outputs/non-regulatory/report_other_calculo_de_riesgo_neto.sql
@@ -10,10 +10,10 @@ select
     net_risk as `Riesgo Neto`,
     reserve_percentage as `% de Reserva`,
     reserve as `Reserva`
-from {{ ref('int_net_risk_calculation') }}
+from {{ ref("int_net_risk_calculation") }}
 left join
-    {{ ref('stg_core_public_ids') }} as credit_facility_public_ids
+    {{ ref("stg_core_public_ids") }} as credit_facility_public_ids
     on line_of_credit = credit_facility_public_ids.target_id
 left join
-    {{ ref('stg_core_public_ids') }} as disbursement_public_ids
+    {{ ref("stg_core_public_ids") }} as disbursement_public_ids
     on disbursement_number = disbursement_public_ids.target_id
