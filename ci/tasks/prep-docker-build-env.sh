9cb114c1e6e54a27501a6afd6f82cc280f40c19a
Kartik Shah <krtk.6160@gmail.com>
2026-01-12 16:22:23 +0530

    ci: more refactor

--- ci/tasks/prep-docker-build-env.sh
+++ ci/tasks/prep-docker-build-env.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+set -eu
+
+# Copy binaries into repo if they exist (RC flow)
+if [[ -d binaries ]]; then
+  cp -r binaries repo/
+fi
+
 if [[ -f version/version ]]; then
   echo "VERSION=$(cat version/version)" >> repo/.env
 fi
 
-echo "COMMITHASH=$(git rev-parse HEAD)" >> repo/.env
+echo "COMMITHASH=$(cd repo && git rev-parse HEAD)" >> repo/.env
 echo "BUILDTIME=$(date -u '+%F-%T')" >> repo/.env
 
-export GH_TOKEN="$(ghtoken generate -b "${GH_APP_PRIVATE_KEY}" -i "${GH_APP_ID}" | jq -r '.token')"
-echo "GH_TOKEN=$GH_TOKEN" >> repo/.env
+# Generate GH_TOKEN only if credentials are provided (release flow, not RC)
+if [[ -n "${GH_APP_ID:-}" ]] && [[ -n "${GH_APP_PRIVATE_KEY:-}" ]]; then
+  export GH_TOKEN="$(ghtoken generate -b "${GH_APP_PRIVATE_KEY}" -i "${GH_APP_ID}" | jq -r '.token')"
+  echo "GH_TOKEN=$GH_TOKEN" >> repo/.env
+fi
