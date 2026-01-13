12fc586c4d3a1d9d3b00fc0a31bc71fb2c99fbba
Kartik Shah <krtk.6160@gmail.com>
2026-01-13 18:48:01 +0530

    ci: update

--- ci/tasks/open-promote-rc-pr.sh
+++ ci/tasks/open-promote-rc-pr.sh
@@ -7,11 +7,18 @@ echo "Promoting to version: $VERSION"
 
 pushd repo
 
+cat <<EOF > ../body.md
+# Promote RC to ${VERSION}
+
+This PR promotes the RC to the final version: ${VERSION}
+EOF
+
 export GH_TOKEN="$(ghtoken generate -b "${GH_APP_PRIVATE_KEY}" -i "${GH_APP_ID}" | jq -r '.token')"
 
 gh pr close ${BOT_BRANCH} || true
 gh pr create \
   --title "chore: promote RC to ${VERSION}" \
   --base ${BRANCH} \
+  --body-file ../body.md \
   --head ${BOT_BRANCH} \
   --label galoybot
