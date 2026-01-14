bc5073fc38f701a172d1b4abca4f97e04e8c4d9e
sebastienverreault <15014256+sebastienverreault@users.noreply.github.com>
2026-01-15 00:05:28 +0900

    feat: last meltano to dagster reports migration (#3369)
    * feat: other non-regulatory reports
    
    * feat: nrp 28 reports
    
    * feat: nrp 41 reports
    
    * feat: nrp 51 reports
    
    * feat: nrsf 03 reports
    
    * fix: nrp 28 format
    
    * fix: nrp 41 format
    
    * fix: nrp 51 format
    
    * fix: nrsf 03 format
    
    * fix: non-reg format
    
    * feat: add uif report doc and data tests

---
+++ dagster/src/dbt_lana_dw/models/outputs/el-salvador-ssf/report_nrp_41_15_socios_sociedades.sql
@@ -0,0 +1,13 @@
+with
+    dummy as (
+
+        select
+            null as `nit_deudor`,
+            null as `nit_socio`,
+            null as `porcentaje_participacion`
+
+    )
+
+select *
+from dummy
+where false
