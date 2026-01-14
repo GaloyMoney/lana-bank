26ee4c7a02eaa5204c8b8f339afce70ed2aaaeaf
Satoshi <sv@galoy.io>
2026-01-14 01:01:04 +0000

    feat: nrp 51 reports

---
+++ dagster/src/dbt_lana_dw/models/outputs/el-salvador-ssf/report_nrp_51_01_saldo_cuenta.sql
@@ -0,0 +1,6 @@
+select
+    cast(format('%.2f', round(`valor`, 2)) as string) as `valor`,
+    right(`id_codigo_cuenta`, 10) as `id_codigo_cuenta`,
+    upper(left(regexp_replace(`nom_cuenta`, r'[&<>"]', '_'), 80)) as `nom_cuenta`
+from
+    {{ ref('int_nrp_51_01_saldo_cuenta') }}
