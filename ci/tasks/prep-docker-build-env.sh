ba0bc55ea853f28f8d57ac015966cca87e80132f
Kartik Shah <krtk.6160@gmail.com>
2026-01-16 12:11:48 +0530

    ci: introduce release candidates and manual releasing

--- ci/tasks/prep-docker-build-env.sh
+++ ci/tasks/prep-docker-build-env.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+set -eu
+
+# Copy binaries into repo if they exist (RC flow)
+if [[ -d binaries ]]; then
+  cp -r binaries repo/
+fi
+
 if [[ -f version/version ]]; then
   echo "VERSION=$(cat version/version)" >> repo/.env
 fi
 
-echo "COMMITHASH=$(git rev-parse HEAD)" >> repo/.env
+echo "COMMITHASH=$(cd repo && git rev-parse HEAD)" >> repo/.env
 echo "BUILDTIME=$(date -u '+%F-%T')" >> repo/.env
-
-export GH_TOKEN="$(ghtoken generate -b "${GH_APP_PRIVATE_KEY}" -i "${GH_APP_ID}" | jq -r '.token')"
-echo "GH_TOKEN=$GH_TOKEN" >> repo/.env
