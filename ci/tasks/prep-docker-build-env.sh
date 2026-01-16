320aa0e225c8fab544d868813c5f8d72fb10c782
Kartik Shah <krtk.6160@gmail.com>
2026-01-16 16:04:45 +0530

    ci: separate `Dockerfile.rc`

--- ci/tasks/prep-docker-build-env.sh
+++ ci/tasks/prep-docker-build-env.sh
@@ -13,3 +13,9 @@ fi
 
 echo "COMMITHASH=$(cd repo && git rev-parse HEAD)" >> repo/.env
 echo "BUILDTIME=$(date -u '+%F-%T')" >> repo/.env
+
+# Generate GH_TOKEN only if credentials are provided (release flow, not RC)
+if [[ -n "${GH_APP_ID:-}" ]] && [[ -n "${GH_APP_PRIVATE_KEY:-}" ]]; then
+  export GH_TOKEN="$(ghtoken generate -b "${GH_APP_PRIVATE_KEY}" -i "${GH_APP_ID}" | jq -r '.token')"
+  echo "GH_TOKEN=$GH_TOKEN" >> repo/.env
+fi
