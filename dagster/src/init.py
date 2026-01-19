4165fde956557253cea137c8750e3d72f388be83
sebastienverreault <15014256+sebastienverreault@users.noreply.github.com>
2026-01-19 19:26:24 +0900

    fix: dagster lana dw json error in udf (#3395)
    * chore: disable udf creation
    
    * chore: remove problematic udf
    
    * fix: problematic udf with sql
    
    * fix: format
    
    * fix: dagster init

--- dagster/src/init.py
+++ dagster/src/init.py
@@ -1,10 +1,6 @@
 import os
 import sys
 
-from dagster_dbt import DbtCliResource
-
-DBT_PROJECT_DIR = "/lana-dw/src/dbt_lana_dw/"
-
 
 def main():
     mode = os.environ.get("DAGSTER_INIT_MODE", "")
@@ -13,10 +9,8 @@ def main():
         print("DAGSTER_INIT_MODE=dry-run, skipping BigQuery UDF creation")
         return
 
-    print("Creating BigQuery UDFs...")
-    dbt_resource = DbtCliResource(project_dir=DBT_PROJECT_DIR)
-    dbt_resource.cli(["run-operation", "create_udfs"]).wait()
-    print("BigQuery UDFs created successfully")
+    # Do the init here
+    print("Init completed")
 
 
 if __name__ == "__main__":
