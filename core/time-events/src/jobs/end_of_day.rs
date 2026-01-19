d0d046a6a25e1dcfb507aa0e0a3bc97b91a80dbe
Lakshyyaa <lakshh113@gmail.com>
2026-01-20 01:09:14 +0530

    feat: introduce time-event crate and eod job

---
+++ core/time-events/src/jobs/end_of_day.rs
@@ -0,0 +1,173 @@
+use async_trait::async_trait;
+use job::{
+    CurrentJob, Job, JobCompletion, JobInitializer, JobRunner, JobSpawner, JobType, RetrySettings,
+};
+use obix::out::{Outbox, OutboxEventMarker};
+use serde::{Deserialize, Serialize};
+use tokio::select;
+
+use audit::{AuditSvc, SystemSubject};
+use authz::PermissionCheck;
+use domain_config::ExposedDomainConfigs;
+
+use crate::{
+    ClosingSchedule,
+    config::{ClosingTimeConfig, TimezoneConfig},
+    event::CoreTimeEvent,
+};
+
+#[derive(Deserialize, Serialize)]
+pub struct EndOfDayBroadcastJobConfig<E>
+where
+    E: OutboxEventMarker<CoreTimeEvent>,
+{
+    pub _phantom: std::marker::PhantomData<E>,
+}
+
+pub struct EndOfDayBroadcastJobInit<E, Perms>
+where
+    E: OutboxEventMarker<CoreTimeEvent>,
+    Perms: PermissionCheck,
+{
+    outbox: Outbox<E>,
+    domain_configs: ExposedDomainConfigs<Perms>,
+}
+
+impl<E, Perms> EndOfDayBroadcastJobInit<E, Perms>
+where
+    E: OutboxEventMarker<CoreTimeEvent>,
+    Perms: PermissionCheck,
+{
+    pub fn new(outbox: &Outbox<E>, domain_configs: &ExposedDomainConfigs<Perms>) -> Self {
+        Self {
+            outbox: outbox.clone(),
+            domain_configs: domain_configs.clone(),
+        }
+    }
+}
+
+pub const END_OF_DAY_BROADCAST_JOB: JobType =
+    JobType::new("cron.core-time-event.end-of-day-broadcast");
+
+impl<E, Perms> JobInitializer for EndOfDayBroadcastJobInit<E, Perms>
+where
+    E: OutboxEventMarker<CoreTimeEvent>,
+    Perms: PermissionCheck,
+    <<Perms as PermissionCheck>::Audit as AuditSvc>::Action:
+        From<domain_config::DomainConfigAction>,
+    <<Perms as PermissionCheck>::Audit as AuditSvc>::Object:
+        From<domain_config::DomainConfigObject>,
+    <<Perms as PermissionCheck>::Audit as AuditSvc>::Subject: SystemSubject,
+{
+    type Config = EndOfDayBroadcastJobConfig<E>;
+
+    fn job_type(&self) -> JobType {
+        END_OF_DAY_BROADCAST_JOB
+    }
+
+    fn init(
+        &self,
+        _: &Job,
+        _: JobSpawner<Self::Config>,
+    ) -> Result<Box<dyn JobRunner>, Box<dyn std::error::Error>> {
+        Ok(Box::new(EndOfDayBroadcastJobRunner {
+            outbox: self.outbox.clone(),
+            domain_configs: self.domain_configs.clone(),
+        }))
+    }
+
+    fn retry_on_error_settings(&self) -> RetrySettings {
+        RetrySettings::repeat_indefinitely()
+    }
+}
+
+pub struct EndOfDayBroadcastJobRunner<E, Perms>
+where
+    E: OutboxEventMarker<CoreTimeEvent>,
+    Perms: PermissionCheck,
+{
+    outbox: Outbox<E>,
+    domain_configs: ExposedDomainConfigs<Perms>,
+}
+
+#[async_trait]
+impl<E, Perms> JobRunner for EndOfDayBroadcastJobRunner<E, Perms>
+where
+    E: OutboxEventMarker<CoreTimeEvent>,
+    Perms: PermissionCheck,
+    <<Perms as PermissionCheck>::Audit as AuditSvc>::Action:
+        From<domain_config::DomainConfigAction>,
+    <<Perms as PermissionCheck>::Audit as AuditSvc>::Object:
+        From<domain_config::DomainConfigObject>,
+    <<Perms as PermissionCheck>::Audit as AuditSvc>::Subject: SystemSubject,
+{
+    async fn run(
+        &self,
+        mut current_job: CurrentJob,
+    ) -> Result<JobCompletion, Box<dyn std::error::Error>> {
+        let system_sub = <<Perms as PermissionCheck>::Audit as AuditSvc>::Subject::system();
+
+        loop {
+            let closing_time_config = self
+                .domain_configs
+                .get::<ClosingTimeConfig>(&system_sub)
+                .await?;
+            let timezone_config = self
+                .domain_configs
+                .get::<TimezoneConfig>(&system_sub)
+                .await?;
+            let closing_time = closing_time_config.value().expect("has default").value;
+            let timezone = timezone_config.value().expect("has default").value;
+
+            let schedule = ClosingSchedule::new(timezone, closing_time);
+            let current_time = current_job.clock().now();
+            let closing_time = schedule.next_closing_from(current_time);
+
+            let duration_until_close = match (closing_time - current_time).to_std() {
+                Ok(duration) => duration,
+                // continue if past date returned and recalculate, not likely
+                Err(err) => {
+                    tracing::warn!(
+                        job_id = %current_job.id(),
+                        job_type = %END_OF_DAY_BROADCAST_JOB,
+                        current_time = %current_time,
+                        closing_time = %closing_time,
+                        error = %err,
+                        "Closing time is in the past, recalculating"
+                    );
+                    continue;
+                }
+            };
+            let clock = current_job.clock().clone();
+            select! {
+                biased;
+
+                _ = current_job.shutdown_requested() => {
+                    tracing::info!(
+                        job_id = %current_job.id(),
+                        job_type = %END_OF_DAY_BROADCAST_JOB,
+                        "Shutdown signal received"
+                    );
+                    return Ok(JobCompletion::RescheduleNow);
+                }
+
+                // TODO: Bring in some special buffer logic, like sleeping less/publishing early
+                _ = clock.sleep(duration_until_close) => {
+                    tracing::debug!(job_id = %current_job.id(), "Sleep completed, continuing");
+                    let mut op = current_job.begin_op().await?;
+                    self.outbox
+                        .publish_persisted_in_op(&mut op, CoreTimeEvent::EndOfDay { closing_time })
+                        .await?;
+                    op.commit().await?;
+
+                    tracing::info!(
+                        job_id = %current_job.id(),
+                        job_type = %END_OF_DAY_BROADCAST_JOB,
+                        closing_time = %closing_time,
+                        "End of day event published"
+                    );
+                }
+            }
+        }
+    }
+}
---
+++ core/time-events/src/lib.rs
@@ -0,0 +1,64 @@
+mod closing_schedule;
+mod config;
+pub mod error;
+mod event;
+mod jobs;
+
+use audit::{AuditSvc, SystemSubject};
+use authz::PermissionCheck;
+use obix::{Outbox, out::OutboxEventMarker};
+use tracing_macros::record_error_severity;
+
+use domain_config::ExposedDomainConfigs;
+
+use crate::{
+    closing_schedule::*,
+    error::TimeEventsError,
+    jobs::end_of_day::{EndOfDayBroadcastJobConfig, EndOfDayBroadcastJobInit},
+};
+
+pub use event::*;
+
+#[derive(Clone)]
+pub struct TimeEvents<Perms>
+where
+    Perms: PermissionCheck,
+{
+    _phantom: std::marker::PhantomData<Perms>,
+}
+
+impl<Perms> TimeEvents<Perms>
+where
+    Perms: PermissionCheck,
+    <<Perms as PermissionCheck>::Audit as AuditSvc>::Action:
+        From<domain_config::DomainConfigAction>,
+    <<Perms as PermissionCheck>::Audit as AuditSvc>::Object:
+        From<domain_config::DomainConfigObject>,
+    <<Perms as PermissionCheck>::Audit as AuditSvc>::Subject: SystemSubject,
+{
+    #[record_error_severity]
+    #[tracing::instrument(name = "core_time_events.init", skip_all)]
+    pub async fn init<E>(
+        domain_configs: &ExposedDomainConfigs<Perms>,
+        jobs: &mut job::Jobs,
+        outbox: &Outbox<E>,
+    ) -> Result<Self, TimeEventsError>
+    where
+        E: OutboxEventMarker<CoreTimeEvent>,
+    {
+        let end_of_day_broadcast_job_spawner =
+            jobs.add_initializer(EndOfDayBroadcastJobInit::new(outbox, domain_configs));
+        end_of_day_broadcast_job_spawner
+            .spawn_unique(
+                job::JobId::new(),
+                EndOfDayBroadcastJobConfig {
+                    _phantom: std::marker::PhantomData,
+                },
+            )
+            .await?;
+
+        Ok(Self {
+            _phantom: std::marker::PhantomData,
+        })
+    }
+}
