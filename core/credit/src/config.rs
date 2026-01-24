2b679ece52ad4f765571ff288afa888caf6b8c7c
Nicolas Burtey <nb@galoy.io>
2026-01-24 15:29:53 -0600

    chore: just needing one value for price interval

--- core/credit/src/config.rs
+++ core/credit/src/config.rs
@@ -9,24 +9,17 @@ use serde::{Deserialize, Serialize};
 pub struct CreditConfig {
     #[serde(default = "default_customer_active_check_enabled")]
     pub customer_active_check_enabled: bool,
-    #[serde(default = "default_collateralization_from_price_job_interval_secs")]
+    #[serde(default = "default_price_job_interval_secs")]
     #[serde_as(as = "serde_with::DurationSeconds<u64>")]
     #[cfg_attr(feature = "json-schema", schemars(with = "u64"))]
-    pub collateralization_from_price_job_interval: std::time::Duration,
-    #[serde(default = "default_pending_collateralization_from_price_job_interval_secs")]
-    #[serde_as(as = "serde_with::DurationSeconds<u64>")]
-    #[cfg_attr(feature = "json-schema", schemars(with = "u64"))]
-    pub pending_collateralization_from_price_job_interval: std::time::Duration,
+    pub price_job_interval: std::time::Duration,
 }
 
 impl Default for CreditConfig {
     fn default() -> Self {
         CreditConfig {
             customer_active_check_enabled: default_customer_active_check_enabled(),
-            collateralization_from_price_job_interval:
-                default_collateralization_from_price_job_interval_secs(),
-            pending_collateralization_from_price_job_interval:
-                default_pending_collateralization_from_price_job_interval_secs(),
+            price_job_interval: default_price_job_interval_secs(),
         }
     }
 }
@@ -35,10 +28,6 @@ fn default_customer_active_check_enabled() -> bool {
     true
 }
 
-fn default_collateralization_from_price_job_interval_secs() -> std::time::Duration {
-    std::time::Duration::from_secs(30)
-}
-
-fn default_pending_collateralization_from_price_job_interval_secs() -> std::time::Duration {
+fn default_price_job_interval_secs() -> std::time::Duration {
     std::time::Duration::from_secs(30)
 }
