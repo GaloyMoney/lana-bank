2bf2695c82c1a220e70fc74e949f17d61f881774
nsandomeno <nsando61@gmail.com>
2026-01-30 12:47:59 -0500

    refactor: mv chart of accounts integration to domain service

---
+++ core/deposit/src/chart_of_accounts_integration/error.rs
@@ -0,0 +1,29 @@
+use thiserror::Error;
+use tracing::Level;
+use tracing_utils::ErrorSeverity;
+
+#[derive(Error, Debug)]
+pub enum ChartOfAccountsIntegrationError {
+    #[error("ChartOfAccountsIntegrationError - AuthorizationError: {0}")]
+    AuthorizationError(#[from] authz::error::AuthorizationError),
+    #[error("ChartOfAccountsIntegrationError - ChartIdMismatch")]
+    ChartIdMismatch,
+    #[error("ChartOfAccountsIntegrationError - DepositLedgerError: {0}")]
+    DepositLedgerError(#[from] crate::ledger::error::DepositLedgerError),
+    #[error("ChartOfAccountsIntegrationError - ChartOfAccountsError: {0}")]
+    ChartOfAccountsError(#[from] core_accounting::chart_of_accounts::error::ChartOfAccountsError),
+    #[error("ChartOfAccountsIntegrationError - ConfigAlreadyExists")]
+    ConfigAlreadyExists,
+}
+
+impl ErrorSeverity for ChartOfAccountsIntegrationError {
+    fn severity(&self) -> Level {
+        match self {
+            Self::AuthorizationError(e) => e.severity(),
+            Self::ChartIdMismatch => Level::ERROR,
+            Self::DepositLedgerError(e) => e.severity(),
+            Self::ChartOfAccountsError(e) => e.severity(),
+            Self::ConfigAlreadyExists => Level::WARN,
+        }
+    }
+}
