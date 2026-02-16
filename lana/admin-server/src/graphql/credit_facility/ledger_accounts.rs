10feb105a71df5529147f395b9aefaf2ebc25393
Jiri Jakes <jiri@jirijakes.com>
2026-02-16 12:52:06 -0300

    chore: Reintroduce collateral accounts in Credit Faciliy GQL

--- lana/admin-server/src/graphql/credit_facility/ledger_accounts.rs
+++ lana/admin-server/src/graphql/credit_facility/ledger_accounts.rs
@@ -13,6 +13,8 @@ pub(super) struct CreditFacilityLedgerAccounts {
     pub disbursed_receivable_overdue_account_id: UUID,
     pub disbursed_defaulted_account_id: UUID,
     pub collateral_account_id: UUID,
+    pub collateral_in_liquidation_account_id: UUID,
+    pub liquidated_collateral_account_id: UUID,
     pub proceeds_from_liquidation_account_id: UUID,
     pub interest_receivable_not_yet_due_account_id: UUID,
     pub interest_receivable_due_account_id: UUID,
@@ -85,6 +87,24 @@ impl CreditFacilityLedgerAccounts {
             .await?
             .expect("Ledger account not found");
         Ok(collateral_account)
+    }
+    async fn collateral_in_liquidation_account(&self, ctx: &Context<'_>) -> Result<LedgerAccount> {
+        let loader = ctx.data_unchecked::<LanaDataLoader>();
+        let collateral_in_liquidation_account = loader
+            .load_one(LedgerAccountId::from(
+                self.collateral_in_liquidation_account_id,
+            ))
+            .await?
+            .expect("Ledger account not found");
+        Ok(collateral_in_liquidation_account)
+    }
+    async fn liquidated_collateral_account(&self, ctx: &Context<'_>) -> Result<LedgerAccount> {
+        let loader = ctx.data_unchecked::<LanaDataLoader>();
+        let liquidated_collateral_account = loader
+            .load_one(LedgerAccountId::from(self.liquidated_collateral_account_id))
+            .await?
+            .expect("Ledger account not found");
+        Ok(liquidated_collateral_account)
     }
     async fn proceeds_from_liquidation_account(&self, ctx: &Context<'_>) -> Result<LedgerAccount> {
         let loader = ctx.data_unchecked::<LanaDataLoader>();
