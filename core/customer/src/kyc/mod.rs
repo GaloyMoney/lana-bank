ef1b3970b56ed170883bd2588e400dc58546fc1e
bodymindarts <justin@galoy.io>
2026-03-06 10:58:02 +0100

    refactor: remove redunadant party lookup

--- core/customer/src/kyc/mod.rs
+++ core/customer/src/kyc/mod.rs
@@ -574,11 +574,7 @@ where
             .customers
             .find_prospect_by_id_without_audit(prospect_id)
             .await?;
-        let party = self
-            .customers
-            .find_party_by_customer_id_without_audit(prospect.party_id)
-            .await?;
-        let level: KycLevel = party.customer_type.into();
+        let level: KycLevel = prospect.customer_type.into();
 
         tracing::info!(
             prospect_id = %prospect_id,
