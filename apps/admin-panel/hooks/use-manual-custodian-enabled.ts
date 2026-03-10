daef25a24dc93379fb9430dad6175938737d692e
Jiri Jakes <jiri@jirijakes.com>
2026-03-10 12:13:12 -0300

    chore: Restructure domain config query

--- apps/admin-panel/hooks/use-manual-custodian-enabled.ts
+++ apps/admin-panel/hooks/use-manual-custodian-enabled.ts
@@ -2,5 +2,5 @@ import { useDomainConfigsQuery } from "@/lib/graphql/generated"
 
 export const useManualCustodianEnabled = (): boolean => {
   const { data } = useDomainConfigsQuery({ variables: { first: 100 } })
-  return data?.domainConfigs.nodes.find((c) => c.key === "enable-manual-custodian")?.value !== false
+  return data?.domainConfigs.nodes.find((c) => c.key === "enable-manual-custodian")?.value === true
 }
